08ee81aa5256d88a86a14765297aba00fc743456
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-07-05 14:44:01 +0900

    initial commit

---
+++ backend/emm-sale/src/main/java/com/emmsale/EmmSaleApplication.java
@@ -0,0 +1,13 @@
+package com.emmsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmmSaleApplication.class, args);
+	}
+
+}
---
+++ backend/emm-sale/src/test/java/com/emmsale/EmmSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
