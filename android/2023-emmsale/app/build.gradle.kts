d1e5383daef3a5f01bc3fad7e49625a9bdd04905
tmdgh1592 <tmdgh1592@naver.com>
2023-07-17 21:53:07 +0900

    chore: fragment-ktx 의존성 추가

--- android/2023-emmsale/app/build.gradle.kts
+++ android/2023-emmsale/app/build.gradle.kts
@@ -46,4 +46,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.fragment:fragment-ktx:1.6.0")
 }
