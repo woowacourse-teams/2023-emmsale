86b06518b22c851673e16071f0ec864a5c8ac4fc
java-saeng <gusdn85554@naver.com>
2023-07-17 22:32:08 +0900

    refactor : MockMvcTestHelper 추가
    #44

--- backend/emm-sale/src/test/java/com/emmsale/career/api/CareerApiTest.java
+++ backend/emm-sale/src/test/java/com/emmsale/career/api/CareerApiTest.java
@@ -4,25 +4,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.emmsale.career.application.CareerService;
-import com.emmsale.member.application.MemberQueryService;
+import com.emmsale.helper.MockMvcTestHelper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(CareerApi.class)
-class CareerApiTest {
-
-  @Autowired
-  private MockMvc mockMvc;
+class CareerApiTest extends MockMvcTestHelper {
 
   @MockBean
   private CareerService careerService;
-
-  @MockBean
-  private MemberQueryService memberQueryService;
 
   @Test
   @DisplayName("커리어를 전제 조회할 수 있으면 200 OK를 반환한다.")
