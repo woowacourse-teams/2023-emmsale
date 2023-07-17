1d381de1aa6b2f371d0cfc34a67f96908bb949ea
tmdgh1592 <tmdgh1592@naver.com>
2023-07-17 22:58:15 +0900

    chore: retrofit, okhttp, kotlin-serialization 의존성 추가

--- android/2023-emmsale/app/build.gradle.kts
+++ android/2023-emmsale/app/build.gradle.kts
@@ -47,4 +47,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.fragment:fragment-ktx:1.6.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 }
