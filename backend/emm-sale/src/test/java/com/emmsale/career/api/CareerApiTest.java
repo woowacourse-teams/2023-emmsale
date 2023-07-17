701365b73a3254da5b0b91afbfea48d129f14da0
java-saeng <gusdn85554@naver.com>
2023-07-17 20:04:03 +0900

    feat : 저장된 활동들 전체 조회 API 구현
    #42

---
+++ backend/emm-sale/src/test/java/com/emmsale/career/api/CareerApiTest.java
@@ -0,0 +1,30 @@
+package com.emmsale.career.api;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.emmsale.career.application.CareerService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(CareerApi.class)
+class CareerApiTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private CareerService careerService;
+
+  @Test
+  @DisplayName("커리어를 전제 조회할 수 있으면 200 OK를 반환한다.")
+  void findAll() throws Exception {
+    // when & then
+    mockMvc.perform(get("/careers"))
+        .andExpect(status().isOk());
+  }
+}
