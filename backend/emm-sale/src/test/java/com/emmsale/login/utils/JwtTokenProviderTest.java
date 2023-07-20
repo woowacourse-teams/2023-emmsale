5abbf9bff2fbc670a25fe43d9f6c473982164ef6
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-07-20 15:46:49 +0900

    test: TokenProvider 테스트 구현 #41

---
+++ backend/emm-sale/src/test/java/com/emmsale/login/utils/JwtTokenProviderTest.java
@@ -0,0 +1,50 @@
+package com.emmsale.login.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+import com.emmsale.base.BaseExceptionType;
+import com.emmsale.helper.ServiceIntegrationTestHelper;
+import com.emmsale.login.exception.LoginException;
+import com.emmsale.login.exception.LoginExceptionType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class JwtTokenProviderTest extends ServiceIntegrationTestHelper {
+
+  @Autowired
+  private JwtTokenProvider jwtTokenProvider;
+
+  @Test
+  @DisplayName("토큰이 유효할 경우 토큰으로부터 사용자의 아이디를 추출하여 반환한다.")
+  void createTokenAndExtractSubjectTest() {
+    // given
+    final String memberId = "1";
+    final String token = jwtTokenProvider.createToken(memberId);
+
+    // when
+    final String subject = jwtTokenProvider.extractSubject(token);
+
+    // then
+    assertThat(subject).isEqualTo(memberId);
+  }
+
+  @Test
+  @DisplayName("토큰이 유효하지 않을 경우 400, BAD_REQUEST가 발생한다.")
+  void createTokenAndExtractSubjectWithExpiredTokenTest() {
+    //given
+    final String token = "invalid_token";
+
+    final LoginExceptionType expectExceptionType = LoginExceptionType.INVALID_ACCESS_TOKEN;
+
+    //when
+    final BaseExceptionType actualExceptionType = assertThrowsExactly(
+        LoginException.class,
+        () -> jwtTokenProvider.extractSubject(token)
+    ).exceptionType();
+
+    //then
+    assertThat(actualExceptionType).isEqualTo(expectExceptionType);
+  }
+}
