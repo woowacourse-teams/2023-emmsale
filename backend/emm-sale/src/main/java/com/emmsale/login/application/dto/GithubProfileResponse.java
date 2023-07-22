c79cb5725d1b3c857aa5c4c38cc2ffab104e35d5
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-07-23 00:08:17 +0900

    fix: default 생성자 오류 수정 #41

--- backend/emm-sale/src/main/java/com/emmsale/login/application/dto/GithubProfileResponse.java
+++ backend/emm-sale/src/main/java/com/emmsale/login/application/dto/GithubProfileResponse.java
@@ -1,9 +1,12 @@
 package com.emmsale.login.application.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
 public class GithubProfileResponse {
