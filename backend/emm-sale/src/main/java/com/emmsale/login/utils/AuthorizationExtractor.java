4d99f76b3bd6b9313f77e336e67d0fb59065df1f
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-07-23 01:07:56 +0900

    refactor: Authorization 추출 메서드 리팩터링 #41

--- backend/emm-sale/src/main/java/com/emmsale/login/utils/AuthorizationExtractor.java
+++ backend/emm-sale/src/main/java/com/emmsale/login/utils/AuthorizationExtractor.java
@@ -1,31 +1,27 @@
 package com.emmsale.login.utils;
 
-import java.util.Enumeration;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 
 public class AuthorizationExtractor {
 
-  private static final String AUTHORIZATION = "Authorization";
   private static final String ACCESS_TOKEN_TYPE =
       AuthorizationExtractor.class.getSimpleName() + ".ACCESS_TOKEN_TYPE";
   private static final String BEARER_TYPE = "Bearer";
 
   public static String extract(final HttpServletRequest request) {
-    final Enumeration<String> headers = request.getHeaders(AUTHORIZATION);
-    while (headers.hasMoreElements()) {
-      final String value = headers.nextElement();
-      if ((value.toLowerCase().startsWith(BEARER_TYPE.toLowerCase()))) {
-        String authHeaderValue = value.substring(BEARER_TYPE.length()).trim();
-        request.setAttribute(ACCESS_TOKEN_TYPE,
-            value.substring(0, BEARER_TYPE.length()).trim());
-        final int commaIndex = authHeaderValue.indexOf(',');
-        if (commaIndex > 0) {
-          authHeaderValue = authHeaderValue.substring(0, commaIndex);
-        }
-        return authHeaderValue;
-      }
+    String authHeaderValue = request.getHeader(HttpHeaders.AUTHORIZATION)
+        .substring(BEARER_TYPE.length()).trim();
+
+    if (authHeaderValue.isBlank()) {
+      return null;
     }
 
-    return null;
+    request.setAttribute(ACCESS_TOKEN_TYPE, BEARER_TYPE);
+    final int commaIndex = authHeaderValue.indexOf(',');
+    if (commaIndex > 0) {
+      authHeaderValue = authHeaderValue.substring(0, commaIndex);
+    }
+    return authHeaderValue;
   }
 }
