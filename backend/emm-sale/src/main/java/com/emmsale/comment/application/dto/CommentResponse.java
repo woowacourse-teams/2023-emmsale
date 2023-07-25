3fcd4b517d91e3b296eec28fbe20ceffee1814dd
java-saeng <gusdn85554@naver.com>
2023-07-25 17:57:22 +0900

    refactor : 정적 팩토리 추가로 인해 private 기본 생성자 추가 및 테스트를 위한 모든 파라미터를 가진 생성자 추가
    #83

--- backend/emm-sale/src/main/java/com/emmsale/comment/application/dto/CommentResponse.java
+++ backend/emm-sale/src/main/java/com/emmsale/comment/application/dto/CommentResponse.java
@@ -1,31 +1,41 @@
 package com.emmsale.comment.application.dto;
 
 import com.emmsale.comment.domain.Comment;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class CommentResponse {
 
-  private final String content;
-  private final Long commentId;
-  private final Long parentId;
-  private final Long eventId;
-  private final boolean isDeleted;
-  private final LocalDateTime createdAt;
+  private static final String DELETE_COMMENT_CONTENT = "삭제된 댓글입니다.";
+
+  private String content;
+  private Long commentId;
+  private Long parentId;
+  private Long eventId;
+  private boolean isDeleted;
+  @JsonFormat(pattern = "yyyy:MM:dd:HH:mm:ss")
+  private LocalDateTime createdAt;
+  @JsonFormat(pattern = "yyyy:MM:dd:HH:mm:ss")
+  private LocalDateTime updatedAt;
 
   public static CommentResponse from(final Comment comment) {
     return new CommentResponse(
         comment.getContent(), comment.getId(),
         getParentId(comment), comment.getEvent().getId(),
-        comment.isDeleted(), comment.getCreatedAt()
+        comment.isDeleted(), comment.getCreatedAt(),
+        comment.getUpdatedAt()
     );
   }
 
   private static Long getParentId(final Comment comment) {
-    if (comment.getParent() == null) {
+    if (comment.isRootComment()) {
       return null;
     }
     return comment.getParent().getId();
