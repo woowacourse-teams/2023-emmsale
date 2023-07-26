d111f81fc2c1ea4f86024a1ff0c4d7c43afa7a1b
hong-sile <gurwns9325@naver.com>
2023-07-26 15:24:20 +0900

    feat: 사용자가 컨퍼런스에 참여자로 추가하는 restdocs Test와 컨트롤러 추가
    #84

--- backend/emm-sale/src/main/java/com/emmsale/event/application/EventService.java
+++ backend/emm-sale/src/main/java/com/emmsale/event/application/EventService.java
@@ -6,6 +6,7 @@ import com.emmsale.event.application.dto.EventDetailResponse;
 import com.emmsale.event.domain.Event;
 import com.emmsale.event.domain.repository.EventRepository;
 import com.emmsale.event.exception.EventException;
+import com.emmsale.member.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,5 +23,9 @@ public class EventService {
     final Event event = eventRepository.findById(id)
         .orElseThrow(() -> new EventException(EVENT_NOT_FOUND_EXCEPTION));
     return EventDetailResponse.from(event);
+  }
+
+  public Long participate(final Long eventId, final Long memberId, final Member member) {
+    return null;
   }
 }
