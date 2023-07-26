1f4157857d3b4b06a83626ade6e70c478080353d
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-07-26 15:40:46 +0900

    refactor: authorization 헤더가 null일 경우 분기 처리 추가
    #104

--- backend/emm-sale/src/main/java/com/emmsale/resolver/MemberArgumentResolver.java
+++ backend/emm-sale/src/main/java/com/emmsale/resolver/MemberArgumentResolver.java
@@ -1,7 +1,5 @@
 package com.emmsale.resolver;
 
-import com.emmsale.login.exception.LoginException;
-import com.emmsale.login.exception.LoginExceptionType;
 import com.emmsale.login.utils.AuthorizationExtractor;
 import com.emmsale.login.utils.JwtTokenProvider;
 import com.emmsale.member.application.MemberQueryService;
@@ -36,10 +34,6 @@ public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
   ) {
     final HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
     final String token = AuthorizationExtractor.extract(request);
-
-    if (token == null || token.isEmpty()) {
-      throw new LoginException(LoginExceptionType.NOT_FOUND_AUTHORIZATION_TOKEN);
-    }
 
     final Long memberId = Long.parseLong(jwtTokenProvider.extractSubject(token));
 
