e907dcaecafa2304b16af63b591e3cbcfa9cc484
tmdgh1592 <tmdgh1592@naver.com>
2023-07-27 10:39:33 +0900

    refactor: 깃허브 로그인 http method를 GET에서 POST로 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/login/LoginService.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/login/LoginService.kt
@@ -2,11 +2,11 @@ package com.emmsale.data.login
 
 import com.emmsale.data.login.dto.LoginApiModel
 import retrofit2.Response
-import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface LoginService {
-    @GET("/login/github/callback")
+    @POST("/login/github/callback")
     suspend fun saveGithubCode(
         @Query("code") code: String
     ): Response<LoginApiModel>
