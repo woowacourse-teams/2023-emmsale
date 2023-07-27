446b8cb2f4ef7b8d2d481927dffb9ef4b5e1964b
amaran-th <songsy405@gmail.com>
2023-07-27 10:15:12 +0900

    refactor: EventTagRepository 경로 수정
    - repository 패키지로 이동
    
    #86

--- backend/emm-sale/src/main/java/com/emmsale/event/domain/repository/EventTagRepository.java
+++ backend/emm-sale/src/main/java/com/emmsale/event/domain/repository/EventTagRepository.java
@@ -1,5 +1,6 @@
-package com.emmsale.event.domain;
+package com.emmsale.event.domain.repository;
 
+import com.emmsale.event.domain.EventTag;
 import com.emmsale.tag.domain.Tag;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
