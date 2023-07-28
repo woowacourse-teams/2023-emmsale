3534dd92ea00e99e65dc6b82f3fc189c935f6e27
ki960213 <ki960213@gmail.com>
2023-07-28 17:53:50 +0900

    feat(MainActivity): 하단 내비게이션 뷰 메뉴 클릭 시 화면이 변경되도록 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/main/MainActivity.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/main/MainActivity.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.emmsale.R
 import com.emmsale.databinding.ActivityMainBinding
+import com.emmsale.presentation.ui.main.events.EventsFragment
+import com.emmsale.presentation.ui.main.profile.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +18,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+        val bnvMain = binding.bnvMain
+
+        bnvMain.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.mi_main_profile -> changeFragment(ProfileFragment())
+                R.id.mi_main_home -> changeFragment(EventsFragment())
+            }
+            return@setOnItemSelectedListener true
+        }
+
+        bnvMain.selectedItemId = R.id.mi_main_home
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fcv_main, fragment).commit()
     }
 
     companion object {
