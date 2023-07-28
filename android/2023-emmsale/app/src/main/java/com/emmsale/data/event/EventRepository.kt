40443eecd048920420074de63e2f0f028f07dbc2
chws0508 <chws0508@naver.com>
2023-07-28 14:48:12 +0900

    refactor : ktlint 수정

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/event/EventRepository.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/event/EventRepository.kt
@@ -4,7 +4,9 @@ import com.emmsale.data.common.ApiResult
 
 interface EventRepository {
     suspend fun getEvents(
-        year: Int, month: Int,
-        status: String, tags: List<String>
+        year: Int,
+        month: Int,
+        status: String,
+        tags: List<String>,
     ): ApiResult<List<Event>>
 }
