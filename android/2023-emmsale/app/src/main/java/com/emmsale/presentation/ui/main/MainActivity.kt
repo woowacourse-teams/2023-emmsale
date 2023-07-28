16e263ef37004e70e630a41dc148dbdfc30af48b
ki960213 <ki960213@gmail.com>
2023-07-28 16:20:34 +0900

    feat(MainActivity): 처음 메인 화면에 가면 프로필 화면이 보이도록 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/main/MainActivity.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/main/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.emmsale.R
+import com.emmsale.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 
     companion object {
---
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/main/profile/ProfileFragment.kt
@@ -0,0 +1,23 @@
+package com.emmsale.presentation.ui.main.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.emmsale.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
