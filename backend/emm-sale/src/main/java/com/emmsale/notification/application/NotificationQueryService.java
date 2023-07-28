64dafe32374b34551499c0560445970dfb1a56ff
java-saeng <gusdn85554@naver.com>
2023-07-28 23:32:04 +0900

    refactor : EntityNotFoundException -> Custom Exception으로 변경

--- backend/emm-sale/src/main/java/com/emmsale/notification/application/NotificationQueryService.java
+++ backend/emm-sale/src/main/java/com/emmsale/notification/application/NotificationQueryService.java
@@ -3,7 +3,8 @@ package com.emmsale.notification.application;
 import com.emmsale.notification.application.dto.NotificationResponse;
 import com.emmsale.notification.domain.Notification;
 import com.emmsale.notification.domain.NotificationRepository;
-import javax.persistence.EntityNotFoundException;
+import com.emmsale.notification.exception.NotificationException;
+import com.emmsale.notification.exception.NotificationExceptionType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,9 +17,10 @@ public class NotificationQueryService {
   private final NotificationRepository notificationRepository;
 
   public NotificationResponse findNotificationBy(final Long notificationId) {
-    //TODO : Notification Exception 이 정해지면 수정
     final Notification savedNotification = notificationRepository.findById(notificationId)
-        .orElseThrow(EntityNotFoundException::new);
+        .orElseThrow(
+            () -> new NotificationException(NotificationExceptionType.NOT_FOUND_NOTIFICATION)
+        );
 
     return NotificationResponse.from(savedNotification);
   }
