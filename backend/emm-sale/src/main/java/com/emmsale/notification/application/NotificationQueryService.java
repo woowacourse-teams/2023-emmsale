e4aee0352930c1912037b7b65f60b7a5961e6e7b
java-saeng <gusdn85554@naver.com>
2023-07-31 12:10:57 +0900

    refactor : Custom Exception 추가 후 Exception 수정
    #133

--- backend/emm-sale/src/main/java/com/emmsale/notification/application/NotificationQueryService.java
+++ backend/emm-sale/src/main/java/com/emmsale/notification/application/NotificationQueryService.java
@@ -1,9 +1,11 @@
 package com.emmsale.notification.application;
 
+import static com.emmsale.notification.exception.NotificationExceptionType.NOT_FOUND_NOTIFICATION;
+
 import com.emmsale.notification.application.dto.NotificationResponse;
 import com.emmsale.notification.domain.Notification;
 import com.emmsale.notification.domain.NotificationRepository;
-import javax.persistence.EntityNotFoundException;
+import com.emmsale.notification.exception.NotificationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,9 +18,8 @@ public class NotificationQueryService {
   private final NotificationRepository notificationRepository;
 
   public NotificationResponse findNotificationBy(final Long notificationId) {
-    //TODO : Notification Exception 이 정해지면 수정
     final Notification savedNotification = notificationRepository.findById(notificationId)
-        .orElseThrow(EntityNotFoundException::new);
+        .orElseThrow(() -> new NotificationException(NOT_FOUND_NOTIFICATION));
 
     return NotificationResponse.from(savedNotification);
   }
