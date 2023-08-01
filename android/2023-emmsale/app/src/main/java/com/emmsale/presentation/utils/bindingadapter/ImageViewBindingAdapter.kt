3913d17d7b617b77f10ccea438e62437f3339c23
ki960213 <ki960213@gmail.com>
2023-08-01 14:55:34 +0900

    feat(ImageViewBindingAdapter): 이미지 뷰 바인딩 어댑터로 에러 이미지를 설정할 수 있도록 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/utils/bindingadapter/ImageViewBindingAdapter.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/utils/bindingadapter/ImageViewBindingAdapter.kt
@@ -1,12 +1,14 @@
 package com.emmsale.presentation.utils.bindingadapter
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-@BindingAdapter("imageUrl")
-fun loadImage(imageView: ImageView, imageUrl: String) {
+@BindingAdapter("imageUrl", "error", requireAll = false)
+fun loadImage(imageView: ImageView, imageUrl: String, errorImage: Drawable?) {
     Glide.with(imageView.context)
         .load(imageUrl)
+        .error(errorImage)
         .into(imageView)
 }
