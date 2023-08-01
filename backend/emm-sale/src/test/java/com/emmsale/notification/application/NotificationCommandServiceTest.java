6f579533bdc420b30b6e11af3995af1a0fec6bf2
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-08-01 10:41:26 +0900

    test: 불필요한 어노테이션 제거
    #141

--- backend/emm-sale/src/test/java/com/emmsale/notification/application/NotificationCommandServiceTest.java
+++ backend/emm-sale/src/test/java/com/emmsale/notification/application/NotificationCommandServiceTest.java
@@ -26,9 +26,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.jdbc.Sql;
 
-@Sql("/data-test.sql")
 class NotificationCommandServiceTest extends ServiceIntegrationTestHelper {
 
   @Autowired
