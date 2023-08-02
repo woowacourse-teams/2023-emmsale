361bb92ad6866063b60a56fce04179a5e812cf24
ki960213 <ki960213@gmail.com>
2023-08-02 14:50:31 +0900

    feat(commentRepository): 특정 댓글의 자식 댓글 조회 기능 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/comment/CommentRepository.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/comment/CommentRepository.kt
@@ -5,4 +5,6 @@ import com.emmsale.data.common.ApiResult
 interface CommentRepository {
 
     suspend fun getComments(eventId: Long): ApiResult<List<Comment>>
+
+    suspend fun getChildComments(commentId: Long): ApiResult<List<Comment>>
 }
--- android/2023-emmsale/app/src/main/java/com/emmsale/data/comment/CommentRepositoryImpl.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/comment/CommentRepositoryImpl.kt
@@ -15,24 +15,55 @@ import kotlinx.coroutines.withContext
 class CommentRepositoryImpl(
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
     private val commentService: CommentService,
-    private val memberRepository: MemberRepository
-) : CommentRepository{
+    private val memberRepository: MemberRepository,
+) : CommentRepository {
 
-    override suspend fun getComments(eventId: Long): ApiResult<List<Comment>> = withContext(dispatcher) {
-        val commentsResponse = commentService.getComments(eventId)
+    override suspend fun getComments(eventId: Long): ApiResult<List<Comment>> =
+        withContext(dispatcher) {
+            val commentsResponse = commentService.getComments(eventId)
 
-        val commentsApiModels = commentsResponse.body() ?: return@withContext ApiError(
-            commentsResponse.code(), commentsResponse.errorBody().toString()
-        )
+            val commentsApiModels = commentsResponse.body() ?: return@withContext ApiError(
+                commentsResponse.code(), commentsResponse.errorBody().toString()
+            )
 
-        val comments = commentsApiModels.map { commentFamilyApiModel ->
-            val parentComment = commentFamilyApiModel.parentComment
-            val childComments = commentFamilyApiModel.childComments
-            val parentCommentAuthor = async { memberRepository.getMember(parentComment.memberId) }
-            val childCommentAuthors = childComments.map {
+            val comments = commentsApiModels.map { commentFamilyApiModel ->
+                val parentComment = commentFamilyApiModel.parentComment
+                val childComments = commentFamilyApiModel.childComments
+                val parentCommentAuthor =
+                    async { memberRepository.getMember(parentComment.memberId) }
+                val childCommentAuthors = childComments.map {
+                    async { memberRepository.getMember(it.memberId) }
+                }
+                val authorResults =
+                    awaitAll(parentCommentAuthor, *childCommentAuthors.toTypedArray())
+                authorResults.forEach {
+                    when (it) {
+                        is ApiError -> return@withContext ApiError(it.code, it.message)
+                        is ApiException -> return@withContext ApiException(it.e)
+                        else -> {}
+                    }
+                }
+                val authors = authorResults.map { it as ApiSuccess }.map { it.data }
+                val parentAuthor = authors[0]
+                val childAuthors = authors.subList(1, authors.size)
+                parentComment.toData(parentAuthor, childComments, childAuthors)
+            }
+
+            ApiSuccess(comments)
+        }
+
+    override suspend fun getChildComments(commentId: Long): ApiResult<List<Comment>> =
+        withContext(dispatcher) {
+            val childCommentsResponse = commentService.getChildComments(commentId)
+
+            val childCommentApiModels = childCommentsResponse.body() ?: return@withContext ApiError(
+                childCommentsResponse.code(), childCommentsResponse.message()
+            )
+
+            val authorsDeferred = childCommentApiModels.map {
                 async { memberRepository.getMember(it.memberId) }
             }
-            val authorResults = awaitAll(parentCommentAuthor, *childCommentAuthors.toTypedArray())
+            val authorResults = awaitAll(*authorsDeferred.toTypedArray())
             authorResults.forEach {
                 when (it) {
                     is ApiError -> return@withContext ApiError(it.code, it.message)
@@ -41,11 +72,9 @@ class CommentRepositoryImpl(
                 }
             }
             val authors = authorResults.map { it as ApiSuccess }.map { it.data }
-            val parentAuthor = authors[0]
-            val childAuthors = authors.subList(1, authors.size)
-            parentComment.toData(parentAuthor, childComments, childAuthors)
+            val childComments = childCommentApiModels.zip(authors) { commentApiModel, author ->
+                commentApiModel.toData(author)
+            }
+            ApiSuccess(childComments)
         }
-
-        ApiSuccess(comments)
-    }
 }
