159eb7970b05afc3a62aa74200af9777a8d2f7ce
tmdgh1592 <tmdgh1592@naver.com>
2023-08-02 17:43:52 +0900

    refactor: EventFragmentStateAdapter 매직 넘버 제거
    Related to : #96

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/main/event/EventFragmentStateAdapter.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/main/event/EventFragmentStateAdapter.kt
@@ -5,11 +5,17 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.emmsale.presentation.ui.main.event.conference.ConferenceFragment
 
 class EventFragmentStateAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 2
+    override fun getItemCount(): Int = TAB_SIZE
 
     override fun createFragment(position: Int): Fragment = when (position) {
-        0 -> ConferenceFragment()
-        1 -> ConferenceFragment()
+        CONFERENCE_FRAGMENT_POSITION -> ConferenceFragment()
+        COMPETITION_FRAGMENT_POSITION -> ConferenceFragment()
         else -> throw IllegalArgumentException("올바르지 않은 fragment position 입니다.")
+    }
+
+    companion object {
+        private const val TAB_SIZE = 2
+        private const val CONFERENCE_FRAGMENT_POSITION = 0
+        private const val COMPETITION_FRAGMENT_POSITION = 1
     }
 }
