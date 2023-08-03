c20de9d8e89ec9916a9ccc376442a8d62ba6299e
ki960213 <ki960213@gmail.com>
2023-08-03 22:02:13 +0900

    feat(ServiceFactory): 프로덕션 서버에서 데이터를 요청하도록 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/common/ServiceFactory.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/common/ServiceFactory.kt
@@ -36,6 +36,6 @@ class ServiceFactory {
     fun <T> create(service: Class<T>): T = retrofit.create(service)
 
     companion object {
-        private const val BASE_URL = "https://kerdy.kro.kr"
+        private const val BASE_URL = "https://prod.kerdy.kro.kr"
     }
 }
