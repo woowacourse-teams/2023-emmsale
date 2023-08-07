f0aefa45710eb659bf4d72f871eaa264746b3ffc
chws0508 <chws0508@naver.com>
2023-08-07 17:22:43 +0900

    feat: 같이가요 탭 모집글 작성 xml 구현

---
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/eventdetail/participant/RecruitmentWritingActivity.kt
@@ -0,0 +1,12 @@
+package com.emmsale.presentation.ui.eventdetail.participant
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.emmsale.R
+
+class RecruitmentWritingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recruitment_writing)
+    }
+}
