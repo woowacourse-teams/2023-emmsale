f458de74801353e463d130cb8585825b0376dac1
hong-sile <gurwns9325@naver.com>
2023-08-07 14:34:12 +0900

    test: 이벤트 상세조회 반환값 변환로직 테스트 추가
    #223

---
+++ backend/emm-sale/src/test/java/com/emmsale/event/application/dto/EventDetailResponseTest.java
@@ -0,0 +1,43 @@
+package com.emmsale.event.application.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.emmsale.event.EventFixture;
+import com.emmsale.event.domain.Event;
+import com.emmsale.event.domain.EventStatus;
+import java.time.LocalDate;
+import java.util.Collections;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EventDetailResponseTest {
+
+  @Test
+  @DisplayName("이벤트를 상세 조회할 값을 변환할 수 있다.")
+  void createEventDetailResponseTest() {
+    //given
+    final Event 구름톤 = EventFixture.구름톤();
+    final LocalDate 날짜 = LocalDate.of(2023, 7, 1);
+    final EventDetailResponse expected = new EventDetailResponse(
+        구름톤.getId(),
+        구름톤.getName(),
+        구름톤.getInformationUrl(),
+        구름톤.getStartDate(),
+        구름톤.getEndDate(),
+        구름톤.getLocation(),
+        EventStatus.UPCOMING.getValue(),
+        Collections.emptyList(),
+        구름톤.getImageUrl(),
+        2,
+        구름톤.getType().toString()
+    );
+
+    //when
+    final EventDetailResponse actual = EventDetailResponse.from(구름톤, 날짜);
+
+    //then
+    assertThat(actual)
+        .usingRecursiveComparison()
+        .isEqualTo(expected);
+  }
+}
