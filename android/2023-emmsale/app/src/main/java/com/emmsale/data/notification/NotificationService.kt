4f3fbaf490344825c52b16df109cb02680af8529
chws0508 <chws0508@naver.com>
2023-08-08 17:33:43 +0900

    refactor: color 포맷 통일

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/notification/NotificationService.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/notification/NotificationService.kt
@@ -4,6 +4,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface NotificationService {
-    @GET("/request-notification")
+    @GET("/request-notifications")
     suspend fun getNotifications(): Response<List<NotificationApiModel>>
 }
