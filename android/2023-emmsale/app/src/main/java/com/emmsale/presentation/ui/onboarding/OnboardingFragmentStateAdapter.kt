7eac79c692487e9e11aa937c7c9c7c68a09c73fc
tmdgh1592 <tmdgh1592@naver.com>
2023-08-08 22:39:01 +0900

    refactor: OnboardingFragmentStateAdapter 리팩터링

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/onboarding/OnboardingFragmentStateAdapter.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/onboarding/OnboardingFragmentStateAdapter.kt
@@ -6,14 +6,22 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class OnboardingFragmentStateAdapter(fragmentActivity: FragmentActivity) :
     FragmentStateAdapter(fragmentActivity) {
-    private val fragments: List<Fragment> = listOf(
-        OnboardingNameFragment(),
-        OnboardingFieldFragment(),
-        OnboardingEducationFragment(),
-        OnboardingClubFragment(),
-    )
 
-    override fun getItemCount(): Int = fragments.size
+    override fun getItemCount(): Int = ONBOARDING_FRAGMENT_SIZE
 
-    override fun createFragment(position: Int): Fragment = fragments[position]
+    override fun createFragment(position: Int): Fragment = when (position) {
+        NAME_FRAGMENT_POSITION -> OnboardingNameFragment()
+        FIELD_FRAGMENT_POSITION -> OnboardingFieldFragment()
+        FIELD_EDUCATION_POSITION -> OnboardingEducationFragment()
+        FIELD_CLUB_POSITION -> OnboardingClubFragment()
+        else -> throw IllegalArgumentException("올바르지 않은 fragment position 입니다.")
+    }
+
+    companion object {
+        private const val ONBOARDING_FRAGMENT_SIZE = 4
+        private const val NAME_FRAGMENT_POSITION = 0
+        private const val FIELD_FRAGMENT_POSITION = 1
+        private const val FIELD_EDUCATION_POSITION = 2
+        private const val FIELD_CLUB_POSITION = 3
+    }
 }
