8b192f0b1afe9c269291096976dc5d0752c70758
chws0508 <chws0508@naver.com>
2023-08-08 16:57:20 +0900

    refactor: notification url 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/notification/NotificationService.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/notification/NotificationService.kt
@@ -4,6 +4,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface NotificationService {
-    @GET("/notifications")
+    @GET("/request-notification")
     suspend fun getNotifications(): Response<List<NotificationApiModel>>
 }
