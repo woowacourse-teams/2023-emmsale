3ed45d5a819b04cdfd745b61177221a0d76e8e93
tmdgh1592 <tmdgh1592@naver.com>
2023-08-08 23:19:55 +0900

    refactor: ConferenceTag 네이밍을 EventTag로 변경
    - 기존 EventTag를 제거하고 상속 관계 제거
    
    Related to : #251

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/eventTag/EventTagRepositoryImpl.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/eventTag/EventTagRepositoryImpl.kt
@@ -3,7 +3,7 @@ package com.emmsale.data.eventTag
 import com.emmsale.data.common.ApiResult
 import com.emmsale.data.common.handleApi
 import com.emmsale.data.conference.EventCategory
-import com.emmsale.data.eventTag.dto.ConferenceTagApiModel
+import com.emmsale.data.eventTag.dto.EventTagApiModel
 import com.emmsale.data.eventTag.mapper.toData
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -16,8 +16,8 @@ class EventTagRepositoryImpl(
     override suspend fun getEventTags(category: EventCategory): ApiResult<List<EventTag>> =
         withContext(dispatcher) {
             handleApi(
-                execute = { eventTagService.getConferenceTags(category.text) },
-                mapToDomain = List<ConferenceTagApiModel>::toData,
+                execute = { eventTagService.getEventTags(category.text) },
+                mapToDomain = List<EventTagApiModel>::toData,
             )
         }
 }
--- android/2023-emmsale/app/src/main/java/com/emmsale/data/eventTag/dto/EventTagApiModel.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/eventTag/dto/EventTagApiModel.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ConferenceTagApiModel(
+data class EventTagApiModel(
     @SerialName("id")
     val id: Long,
     @SerialName("name")
