111d9dcf09a550bafe82eeb5f08f04f8c8c7a363
chws0508 <chws0508@naver.com>
2023-08-10 22:59:17 +0900

    refactor : mapper 분리에 대한 RepositoryImpl 변경사항 적용

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/EventDetailRepositoryImpl.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/EventDetailRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.emmsale.data.eventdetail
 import com.emmsale.data.common.ApiResult
 import com.emmsale.data.common.handleApi
 import com.emmsale.data.eventdetail.dto.EventDetailApiModel
+import com.emmsale.data.eventdetail.mapper.toData
 
 class EventDetailRepositoryImpl(
     private val eventDetailService: EventDetailService,
