7bf4d1c3b7741ff252554b063dd01e0371fef3c5
chws0508 <chws0508@naver.com>
2023-08-10 22:48:22 +0900

    refactor :  EventDetail Mapper 분리

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/dto/EventDetailApiModel.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/dto/EventDetailApiModel.kt
@@ -1,10 +1,7 @@
 package com.emmsale.data.eventdetail.dto
 
-import com.emmsale.data.eventdetail.EventDetail
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 @Serializable
 data class EventDetailApiModel(
@@ -30,23 +27,4 @@ data class EventDetailApiModel(
     val remainingDays: Int,
     @SerialName("type")
     val type: String,
-) {
-    fun toData(): EventDetail = EventDetail(
-        id = id,
-        name = name,
-        status = status,
-        location = location,
-        startDate = startDate.toLocalDateTime(),
-        endDate = endDate.toLocalDateTime(),
-        informationUrl = informationUrl,
-        tags = tags,
-        imageUrl = imageUrl,
-        remainingDays = remainingDays,
-        type = type,
-    )
-
-    private fun String.toLocalDateTime(): LocalDateTime {
-        val format = DateTimeFormatter.ofPattern("yyyy:MM:dd:HH:mm:ss")
-        return LocalDateTime.parse(this, format)
-    }
-}
+)
