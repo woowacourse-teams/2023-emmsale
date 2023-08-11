10c3c455aabebb3b7cbbc8f2b32d746441e4a817
hong-sile <gurwns9325@naver.com>
2023-08-11 15:38:48 +0900

    refactor: description, createdAt 삭제
    #292

--- backend/emm-sale/src/main/java/com/emmsale/event/application/dto/RecruitmentPostResponse.java
+++ backend/emm-sale/src/main/java/com/emmsale/event/application/dto/RecruitmentPostResponse.java
@@ -2,24 +2,23 @@ package com.emmsale.event.application.dto;
 
 import com.emmsale.event.domain.RecruitmentPost;
 import com.emmsale.member.domain.Member;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @RequiredArgsConstructor
 @Getter
 public class RecruitmentPostResponse {
 
+  private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
   private final Long id;
   private final Long memberId;
   private final String name;
   private final String imageUrl;
-  private final String description;
   private final String content;
-  @JsonFormat(pattern = "yyyy.MM.dd")
-  private final LocalDate createdAt;
-  @JsonFormat(pattern = "yyyy.MM.dd")
+  @DateTimeFormat(pattern = DATE_TIME_FORMAT)
   private final LocalDate updatedAt;
 
   public static RecruitmentPostResponse from(final RecruitmentPost recruitmentPost) {
@@ -29,9 +28,7 @@ public class RecruitmentPostResponse {
         member.getId(),
         member.getName(),
         member.getImageUrl(),
-        member.getDescription(),
         recruitmentPost.getContent(),
-        recruitmentPost.getCreatedAt().toLocalDate(),
         recruitmentPost.getUpdatedAt().toLocalDate()
     );
   }
