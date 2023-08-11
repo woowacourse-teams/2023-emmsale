c53f59e76c23006763f72efe4e72dafd14a4c5c0
Choi Woo Seok <chws0508@naver.com>
2023-08-11 16:00:42 +0900

    refactor :  EventDetail 코드 리팩토링 (#282)
    * refactor :  EventDetail 코드 리팩토링
    
    * refactor :  EventDetail 코드 리팩토링
    
    * refactor :  EventDetail 코드 리팩토링
    
    * refactor :  EventDetail Mapper 분리
    
    * refactor : 버튼 클릭 함수명 변경
    
    * refactor : viewModel 생성시에 한번만 fetch 하도록 수정
    
    * refactor : mapper 분리에 대한 RepositoryImpl 변경사항 적용
    
    * refactor : init 함수와 프로퍼티 순서 바꿈
    
    * refactor : 부나 피드백 반영
    
    - 함수명 변경
    - strings.xml 포맷 변경
    - initFragmentStateAdapter 리스트 생성하여 attach 하도록 변경
    - imageUrl 을 postImageUrl 로 좀더 명확하게 변경
    
    * refactor : EventDetailApiModel 속성 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/EventDetail.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/EventDetail.kt
@@ -1,15 +1,17 @@
 package com.emmsale.data.eventdetail
 
+import java.time.LocalDateTime
+
 data class EventDetail(
     val id: Long,
     val name: String,
     val status: String,
     val location: String,
-    val startDate: String,
-    val endDate: String,
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
     val informationUrl: String,
     val tags: List<String>,
-    val imageUrl: String?,
+    val postImageUrl: String?,
     val remainingDays: Int,
     val type: String,
 )
---
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/mapper/EventDetailMapper.kt
@@ -0,0 +1,25 @@
+package com.emmsale.data.eventdetail.mapper
+
+import com.emmsale.data.eventdetail.EventDetail
+import com.emmsale.data.eventdetail.dto.EventDetailApiModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+fun EventDetailApiModel.toData(): EventDetail = EventDetail(
+    id = id,
+    name = name,
+    status = status,
+    location = location,
+    startDate = startDate.toLocalDateTime(),
+    endDate = endDate.toLocalDateTime(),
+    informationUrl = informationUrl,
+    tags = tags,
+    postImageUrl = imageUrl,
+    remainingDays = remainingDays,
+    type = type,
+)
+
+private fun String.toLocalDateTime(): LocalDateTime {
+    val format = DateTimeFormatter.ofPattern("yyyy:MM:dd:HH:mm:ss")
+    return LocalDateTime.parse(this, format)
+}
