bab6f077a719b2f91db83e22f30ede0feba3a2cb
hong-sile <gurwns9325@naver.com>
2023-08-11 20:54:20 +0900

    feat: 태그 전체조회 기능 구현
    #144

--- backend/emm-sale/src/main/java/com/emmsale/tag/application/TagQueryService.java
+++ backend/emm-sale/src/main/java/com/emmsale/tag/application/TagQueryService.java
@@ -1,11 +1,15 @@
 package com.emmsale.tag.application;
 
+import static java.util.stream.Collectors.toUnmodifiableList;
+
 import com.emmsale.tag.application.dto.TagResponse;
 import com.emmsale.tag.domain.TagRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class TagQueryService {
@@ -13,6 +17,9 @@ public class TagQueryService {
   private final TagRepository tagRepository;
 
   public List<TagResponse> findAll() {
-    return null;
+    return tagRepository.findAll()
+        .stream()
+        .map(TagResponse::from)
+        .collect(toUnmodifiableList());
   }
 }
--- backend/emm-sale/src/main/java/com/emmsale/tag/application/dto/TagResponse.java
+++ backend/emm-sale/src/main/java/com/emmsale/tag/application/dto/TagResponse.java
@@ -1,5 +1,6 @@
 package com.emmsale.tag.application.dto;
 
+import com.emmsale.tag.domain.Tag;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +10,8 @@ public class TagResponse {
 
   private final Long id;
   private final String name;
+
+  public static TagResponse from(final Tag tag) {
+    return new TagResponse(tag.getId(), tag.getName());
+  }
 }
