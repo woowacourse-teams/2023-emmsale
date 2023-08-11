6fbb8b15fa4d6dcb7de22a1c4d112ae03a2ff2ad
chws0508 <chws0508@naver.com>
2023-08-11 15:59:51 +0900

    refactor : EventDetailApiModel 속성 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/dto/EventDetailApiModel.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/dto/EventDetailApiModel.kt
@@ -9,22 +9,30 @@ data class EventDetailApiModel(
     val id: Long,
     @SerialName("name")
     val name: String,
-    @SerialName("status")
-    val status: String,
-    @SerialName("location")
-    val location: String,
+    @SerialName("informationUrl")
+    val informationUrl: String,
     @SerialName("startDate")
     val startDate: String,
     @SerialName("endDate")
     val endDate: String,
-    @SerialName("informationUrl")
-    val informationUrl: String,
+    @SerialName("applyStartDate")
+    val applyStartDate: String,
+    @SerialName("applyEndDate")
+    val applyEndDate: String,
+    @SerialName("location")
+    val location: String,
+    @SerialName("status")
+    val status: String,
+    @SerialName("applyStatus")
+    val applyStatus: String,
     @SerialName("tags")
     val tags: List<String>,
     @SerialName("imageUrl")
     val imageUrl: String?,
     @SerialName("remainingDays")
     val remainingDays: Int,
+    @SerialName("applyRemainingDays")
+    val applyRemainingDays: Int,
     @SerialName("type")
     val type: String,
 )
