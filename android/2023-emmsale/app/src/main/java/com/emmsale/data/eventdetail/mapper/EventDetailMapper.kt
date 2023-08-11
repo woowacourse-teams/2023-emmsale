de5959d88f51835d775431e5527422135a2f5d5d
ki960213 <ki960213@gmail.com>
2023-08-11 18:12:49 +0900

    refactor(mapper): 문자열 확장 함수의 접근 제한자를 private으로 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/mapper/EventDetailMapper.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/eventdetail/mapper/EventDetailMapper.kt
@@ -28,14 +28,14 @@ private fun String.toLocalDateTime(): LocalDateTime {
     return LocalDateTime.parse(this, format)
 }
 
-fun String.toKoreanStatus(): String = when (this) {
+private fun String.toKoreanStatus(): String = when (this) {
     "IN_PROGRESS" -> "진행 중"
     "UPCOMING" -> "진행 예정"
     "ENDED" -> "마감"
     else -> throw IllegalArgumentException("행사 상세 정보의 상태를 변환하는 데 실패했습니다. api 스펙을 다시 확인해주세요.")
 }
 
-fun String.toKoreanApplyStatus(): String = when (this) {
+private fun String.toKoreanApplyStatus(): String = when (this) {
     "IN_PROGRESS" -> "진행 중"
     "UPCOMING" -> "진행 예정"
     "ENDED" -> "마감"
