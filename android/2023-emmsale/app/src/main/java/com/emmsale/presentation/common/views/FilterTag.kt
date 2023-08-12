1eb5855ba2fab46c8d47e6020e6407078f34332d
buna <tmdgh1592@naver.com>
2023-08-12 18:00:37 +0900

    chore: FilterTagStyle Resource 버그 수정

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/views/FilterTag.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/views/FilterTag.kt
@@ -1,24 +1,34 @@
 package com.emmsale.presentation.common.views
 
 import android.content.Context
+import android.graphics.Canvas
 import android.util.AttributeSet
+import android.view.ViewGroup.MarginLayoutParams
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import com.emmsale.R
+import com.emmsale.presentation.common.extension.dp
 
 class FilterTag : AppCompatButton {
     constructor(context: Context) : super(context, null, R.attr.FilterTagStyle)
     constructor(context: Context, attrs: AttributeSet?) : super(
         context,
         attrs,
-        R.attr.FilterTagStyle
+        R.attr.FilterTagStyle,
     )
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
-        defStyleAttr
+        defStyleAttr,
     )
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val marginLayoutParams = MarginLayoutParams::class.java.cast(layoutParams)
+        marginLayoutParams.marginEnd = 5.dp
+        layoutParams = marginLayoutParams
+    }
 }
 
 fun Context.filterChipOf(
