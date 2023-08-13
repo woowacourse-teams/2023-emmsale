7a4a36f6f0df5d00d5ebc7ca5e3b0196f135c9fd
java-saeng <gusdn85554@naver.com>
2023-08-13 15:58:08 +0900

    refactor : 알림 이벤트 클래스 따로 분기
    #297

--- backend/emm-sale/src/test/java/com/emmsale/notification/application/UpdateNotificationCommandServiceTest.java
+++ backend/emm-sale/src/test/java/com/emmsale/notification/application/UpdateNotificationCommandServiceTest.java
@@ -1,17 +1,21 @@
 package com.emmsale.notification.application;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.emmsale.comment.domain.Comment;
-import com.emmsale.comment.event.UpdateNotificationEvent;
+import com.emmsale.event.EventFixture;
+import com.emmsale.event.domain.Event;
+import com.emmsale.event.domain.repository.EventRepository;
 import com.emmsale.helper.ServiceIntegrationTestHelper;
+import com.emmsale.member.domain.Member;
+import com.emmsale.member.domain.MemberRepository;
+import com.emmsale.member.exception.MemberException;
+import com.emmsale.member.exception.MemberExceptionType;
 import com.emmsale.notification.domain.UpdateNotification;
 import com.emmsale.notification.domain.UpdateNotificationRepository;
+import com.emmsale.notification.domain.UpdateNotificationType;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -24,36 +28,55 @@ class UpdateNotificationCommandServiceTest extends ServiceIntegrationTestHelper 
   private UpdateNotificationCommandService updateNotificationCommandService;
   @Autowired
   private UpdateNotificationRepository updateNotificationRepository;
-  private FirebaseCloudMessageClient mockingFirebaseCloudMessageClient;
+  @Autowired
+  private EventRepository eventRepository;
+  @Autowired
+  private MemberRepository memberRepository;
+
+  private UpdateNotification 이벤트_알림;
+  private Member member;
 
   @BeforeEach
   void setUp() {
-    mockingFirebaseCloudMessageClient = mock(FirebaseCloudMessageClient.class);
+    final Event event = eventRepository.save(EventFixture.인프콘_2023());
+    member = memberRepository.findById(1L).get();
 
-    updateNotificationCommandService = new UpdateNotificationCommandService(
-        updateNotificationRepository, mockingFirebaseCloudMessageClient
+    이벤트_알림 = updateNotificationRepository.save(
+        new UpdateNotification(
+            member.getId(),
+            event.getId(),
+            UpdateNotificationType.EVENT,
+            LocalDateTime.now()
+        )
     );
   }
 
   @Test
-  @DisplayName("createUpdateNotification() : UpdateNotificationEvent 이벤트가 발생했을 때 해당 메서드가 호출된다.")
-  void test_createUpdateNotification() throws Exception {
-    //given
-    final UpdateNotificationEvent event = new UpdateNotificationEvent(
-        1L,
-        2L,
-        Comment.class.getName(),
-        LocalDateTime.now()
-    );
-
+  @DisplayName("read() : 로그인 한 사용자와 알림 받는 사람이 일치할 때, 해당 알림을 읽음 상태로 변경할 수 있다.")
+  void test_read() throws Exception {
     //when
-    updateNotificationCommandService.createUpdateNotification(event);
+    final Long notificationId = 이벤트_알림.getId();
+    updateNotificationCommandService.read(member, notificationId);
 
     //then
+    final UpdateNotification updatedNotification =
+        updateNotificationRepository.findById(notificationId).get();
+
     assertAll(
-        () -> assertEquals(1, updateNotificationRepository.findAll().size()),
-        () -> verify(mockingFirebaseCloudMessageClient, times(1))
-            .sendMessageTo((UpdateNotification) any())
+        () -> assertEquals(notificationId, updatedNotification.getId()),
+        () -> assertTrue(updatedNotification.isRead())
     );
+  }
+
+  @Test
+  @DisplayName("read() : 토큰 주인과 로그인 한 사용자가 다르면 읽음 상태를 변경할 수 없다.")
+  void test_read_not_matching_token_and_login() throws Exception {
+    //given
+    final Member otherMember = memberRepository.findById(2L).get();
+
+    //when & then
+    assertThatThrownBy(() -> updateNotificationCommandService.read(otherMember, 이벤트_알림.getId()))
+        .isInstanceOf(MemberException.class)
+        .hasMessage(MemberExceptionType.NOT_MATCHING_TOKEN_AND_LOGIN.errorMessage());
   }
 }
