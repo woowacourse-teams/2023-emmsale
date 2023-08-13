b2c43fc4e00381f197ce6d60977781a6638a5327
ki960213 <ki960213@gmail.com>
2023-08-13 14:29:15 +0900

    feat(ProfileActivity): 프로필 화면을 실행시키는 정적 팩토리 메서드 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/profile/ProfileActivity.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/profile/ProfileActivity.kt
@@ -1,5 +1,7 @@
 package com.emmsale.presentation.ui.profile
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.emmsale.R
@@ -8,5 +10,18 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+    }
+
+    companion object {
+        private const val KEY_MEMBER_ID = "KEY_MEMBER_ID"
+
+        fun startActivity(context: Context, memberId: Long) {
+            val intent = Intent(context, ProfileActivity::class.java).apply {
+                putExtra(KEY_MEMBER_ID, memberId)
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }
+
+            context.startActivity(intent)
+        }
     }
 }
