b18ab196fd89a42ba472be574f8562e3cb500a1a
buna <tmdgh1592@naver.com>
2023-08-13 15:15:55 +0900

    refactor: NotificationBoxFragmentStateAdapter의 매직 리터럴 제거

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/notificationBox/NotificationBoxFragmentStateAdapter.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/notificationBox/NotificationBoxFragmentStateAdapter.kt
@@ -7,19 +7,22 @@ import com.emmsale.presentation.ui.notificationBox.primaryNotification.PrimaryNo
 import com.emmsale.presentation.ui.notificationBox.recruitmentNotification.RecruitmentNotificationFragment
 
 class NotificationBoxFragmentStateAdapter(
-    activity: FragmentActivity
+    activity: FragmentActivity,
 ) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int = NOTIFICATION_BOX_TAB_COUNT
 
     override fun createFragment(position: Int): Fragment = when (position) {
         RECRUITMENT_NOTIFICATION_TAB_POSITION -> RecruitmentNotificationFragment()
         PRIMARY_NOTIFICATION_TAB_POSITION -> PrimaryNotificationFragment()
-        else -> throw IllegalArgumentException("Invalid position: $position")
+        else -> throw IllegalArgumentException(INVALID_FRAGMENT_POSITION_MESSAGE.format(position))
     }
 
     companion object {
         private const val NOTIFICATION_BOX_TAB_COUNT = 2
         private const val RECRUITMENT_NOTIFICATION_TAB_POSITION = 0
         private const val PRIMARY_NOTIFICATION_TAB_POSITION = 1
+
+        private const val INVALID_FRAGMENT_POSITION_MESSAGE =
+            "%d는 올바르지 않은 Fragment Position입니다."
     }
 }
