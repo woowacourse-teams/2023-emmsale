371a1d884ea84ee4f37ac25de2e4aa42123b41ab
ki960213 <ki960213@gmail.com>
2023-08-14 12:24:19 +0900

    refactor(BottomDialogMenuItem): BottomDialogMenuItem과 관련된 크기를 리소스에 추가

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/views/bottomMenuDialog/BottomDialogMenuItem.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/views/bottomMenuDialog/BottomDialogMenuItem.kt
@@ -2,15 +2,18 @@ package com.emmsale.presentation.common.views.bottomMenuDialog
 
 import android.content.Context
 import android.graphics.Typeface
-import androidx.annotation.Dimension
 import androidx.appcompat.widget.AppCompatTextView
-import com.emmsale.presentation.common.extension.dp
+import com.emmsale.R
+import kotlin.math.roundToInt
 
 class BottomDialogMenuItem(context: Context) : AppCompatTextView(context) {
 
     init {
         setTypeface(null, Typeface.BOLD)
-        setTextSize(Dimension.SP, 14F)
-        setPadding(0, 21.dp, 0, 21.dp)
+        textSize = resources.getDimension(R.dimen.textsize_bottomdialogmenuitem_title)
+        val verticalPadding =
+            resources.getDimension(R.dimen.padding_bottommenudialog_menu_item_vertical)
+                .roundToInt()
+        setPadding(0, verticalPadding, 0, verticalPadding)
     }
 }
