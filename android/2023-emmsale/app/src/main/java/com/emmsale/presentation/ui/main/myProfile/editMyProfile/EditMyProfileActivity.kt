f88f5ef73b73119f0fd40a44496574bf389af712
ki960213 <ki960213@gmail.com>
2023-08-14 20:24:30 +0900

    feat(EditMyProfileActivity): 내 프로필 수정 화면의 툴바 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/main/myProfile/editMyProfile/EditMyProfileActivity.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/main/myProfile/editMyProfile/EditMyProfileActivity.kt
@@ -4,12 +4,23 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.emmsale.R
+import com.emmsale.databinding.ActivityEditMyProfileBinding
 
 class EditMyProfileActivity : AppCompatActivity() {
+
+    private val binding: ActivityEditMyProfileBinding by lazy {
+        ActivityEditMyProfileBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_my_profile)
+        setContentView(binding.root)
+
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        binding.tbEditmyprofileToolbar.setNavigationOnClickListener { finish() }
     }
 
     companion object {
