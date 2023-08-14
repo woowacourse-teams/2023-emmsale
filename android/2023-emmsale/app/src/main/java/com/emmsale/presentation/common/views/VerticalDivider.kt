7756f8d58774220813252670bacac268897d073e
ki960213 <ki960213@gmail.com>
2023-08-14 18:39:17 +0900

    feat(SettingFragment): 설정 화면 틀 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/views/VerticalDivider.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/views/VerticalDivider.kt
@@ -1,10 +1,14 @@
 package com.emmsale.presentation.common.views
 
 import android.content.Context
+import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 import com.emmsale.R
 
-class VerticalDivider(context: Context) : AppCompatImageView(context) {
+class VerticalDivider(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : AppCompatImageView(context, attrs) {
     init {
         setBackgroundResource(R.drawable.bg_all_vertical_divider)
     }
