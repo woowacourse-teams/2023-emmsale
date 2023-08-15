cabf2a09907dfbafaa6de3fd734d9a025f2dff3d
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-08-15 19:09:55 +0900

    refactor: 불필요한 쿼리 제거
    #274

--- backend/emm-sale/src/main/java/com/emmsale/scrap/domain/ScrapRepository.java
+++ backend/emm-sale/src/main/java/com/emmsale/scrap/domain/ScrapRepository.java
@@ -2,14 +2,12 @@ package com.emmsale.scrap.domain;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
 
   boolean existsByMemberIdAndEventId(Long memberId, Long eventId);
 
-  @Query("select s from Scrap s where s.memberId = :memberId")
   List<Scrap> findAllByMemberId(@Param("memberId") Long memberId);
 
 }
