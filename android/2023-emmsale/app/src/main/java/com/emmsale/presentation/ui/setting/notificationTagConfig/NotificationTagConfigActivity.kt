a29dafa83cc86a7f614650a2c7d376f69fd1e664
buna <tmdgh1592@naver.com>
2023-08-16 16:24:00 +0900

    feat: 태그 알림 화면에서 뒤로가기 할 때 확인 다이얼로그 보여주는 기능 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/setting/notificationTagConfig/NotificationTagConfigActivity.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/setting/notificationTagConfig/NotificationTagConfigActivity.kt
@@ -5,7 +5,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.emmsale.R
 import com.emmsale.databinding.ActivityNotificationTagConfigBinding
+import com.emmsale.presentation.common.views.ConfirmDialog
 
 class NotificationTagConfigActivity : AppCompatActivity() {
     private val viewModel: NotificationTagConfigViewModel by viewModels { NotificationTagConfigViewModel.factory }
@@ -22,6 +24,26 @@ class NotificationTagConfigActivity : AppCompatActivity() {
     private fun initView() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        initToolbarNavigationClickListener()
+    }
+
+    private fun initToolbarNavigationClickListener() {
+        binding.tbNotificationTagConfig.setNavigationOnClickListener {
+            showFinishConfirmDialog()
+        }
+    }
+
+    private fun showFinishConfirmDialog() {
+        ConfirmDialog(
+            context = this,
+            title = getString(R.string.notificationtagconfig_finish_dialog_title),
+            message = getString(R.string.notificationtagconfig_finish_dialog_message),
+            onPositiveButtonClick = { finish() },
+        ).show()
     }
 
     companion object {
