55341af4f54537c8aa116f4396572c5004623e85
buna <tmdgh1592@naver.com>
2023-08-16 16:33:34 +0900

    fix: 알림 목록 삭제하는 기능 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/notification/NotificationService.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/notification/NotificationService.kt
@@ -6,8 +6,8 @@ import com.emmsale.data.notification.updated.dto.UpdatedNotificationApiModel
 import com.emmsale.data.notification.updated.dto.UpdatedNotificationDeleteRequestModel
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.HTTP
 import retrofit2.http.PATCH
 import retrofit2.http.PUT
 import retrofit2.http.Path
@@ -40,7 +40,7 @@ interface NotificationService {
         @Path("update-notification-id") notificationId: Long,
     ): Response<Unit>
 
-    @DELETE("/update-notifications")
+    @HTTP(method = "DELETE", path = "/update-notifications", hasBody = true)
     suspend fun deleteNotification(
         @Body notificationIds: UpdatedNotificationDeleteRequestModel,
     ): Response<Unit>
