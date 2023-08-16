49c84a264ab78bac7d60ba0b680202b9e847ff30
hong-sile <gurwns9325@naver.com>
2023-08-16 23:28:26 +0900

    feat: info, warn, error 로그가 각자의 로그 파일에 저장되도록 기능 구현
    #287

--- backend/emm-sale/src/main/java/com/emmsale/GlobalControllerAdvice.java
+++ backend/emm-sale/src/main/java/com/emmsale/GlobalControllerAdvice.java
@@ -3,7 +3,8 @@ package com.emmsale;
 import com.emmsale.base.BaseException;
 import com.emmsale.base.BaseExceptionType;
 import java.util.stream.Collectors;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-@Slf4j
 public class GlobalControllerAdvice {
+
+  private final Logger warnLogger = LoggerFactory.getLogger("globalWarnLogger");
+  private final Logger errorLogger = LoggerFactory.getLogger("globalErrorLogger");
 
   @ExceptionHandler(BaseException.class)
   public ResponseEntity<ExceptionResponse> handleException(final BaseException e) {
@@ -23,11 +26,12 @@ public class GlobalControllerAdvice {
     final BaseExceptionType type = e.exceptionType();
 
     if (type.httpStatus().is5xxServerError()) {
-      log.error("[ERROR] MESSAGE : {}, 로그 캡처와 함께 서버 개발자에게 연락주세요 : ", type.errorMessage(), e);
+      errorLogger.error("[ERROR] MESSAGE : {}, 로그 캡처와 함께 서버 개발자에게 연락주세요 : ", type.errorMessage(),
+          e);
       return new ResponseEntity<>(ExceptionResponse.from(e), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    log.warn("[WARN] MESSAGE: {}", type.errorMessage());
+    warnLogger.warn("[WARN] MESSAGE: {}", type.errorMessage());
     return new ResponseEntity<>(ExceptionResponse.from(e), type.httpStatus());
   }
 
@@ -37,7 +41,7 @@ public class GlobalControllerAdvice {
     final String message = e.getBindingResult().getAllErrors().stream()
         .map(DefaultMessageSourceResolvable::getDefaultMessage)
         .collect(Collectors.joining("\n"));
-    log.warn("[WARN] MESSAGE: {}", message);
+    warnLogger.warn("[WARN] MESSAGE: {}", message);
     return new ResponseEntity<>(new ExceptionResponse(message), HttpStatus.BAD_REQUEST);
   }
 
@@ -45,7 +49,7 @@ public class GlobalControllerAdvice {
   public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(
       final HttpMessageNotReadableException e) {
     final String message = "입력 형식이 올바르지 않습니다.";
-    log.warn("[WARN] MESSAGE: " + message);
+    warnLogger.warn("[WARN] MESSAGE: " + message);
     return new ResponseEntity<>(new ExceptionResponse(message), HttpStatus.BAD_REQUEST);
   }
 
@@ -53,13 +57,13 @@ public class GlobalControllerAdvice {
   public ResponseEntity<ExceptionResponse> handleMissingServletRequestParameterException(
       final MissingServletRequestParameterException e) {
     final String message = "요청 파라미터가 올바르지 않습니다..";
-    log.warn("[WARN] MESSAGE: " + message);
+    warnLogger.warn("[WARN] MESSAGE: " + message);
     return new ResponseEntity<>(new ExceptionResponse(message), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ExceptionResponse> handleException(final Exception exception) {
-    log.error("[ERROR] 로그 캡처와 함께 서버 개발자에게 연락주세요 : ", exception);
+    errorLogger.error("[ERROR] 로그 캡처와 함께 서버 개발자에게 연락주세요 : ", exception);
     return new ResponseEntity<>(
         ExceptionResponse.from(exception),
         HttpStatus.INTERNAL_SERVER_ERROR
