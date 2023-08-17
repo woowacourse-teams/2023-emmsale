7ee26c5d0c42a8d02ba1817c973ed8a3d0f26ed0
buna <tmdgh1592@naver.com>
2023-08-17 22:13:58 +0900

    feat: 로깅 전략 함수화

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/firebase/analytics/KerdyAnalytics.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/firebase/analytics/KerdyAnalytics.kt
@@ -5,6 +5,28 @@ import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.ParametersBuilder
 import com.google.firebase.analytics.ktx.logEvent
 
+/**
+ * ANALYTICS EVENT NAMES
+ *
+ * */
+private const val EVENT_CLICK = "event_click"
+private const val COMMENT = "button_click"
+private const val RECRUITMENT = "recruitment"
+private const val WRITE_RECRUITMENT = "write_recruitment"
+private const val INTEREST_TAGS = "interest_tags"
+private const val CHANGE_CONFIG = "change_config"
+
+/**
+ *  ANALYTICS PARAM KEYS
+ *
+ *  */
+private const val EVENT_NAME = "event_name"
+private const val EVENT_ID = "event_id"
+private const val USER_ID = "user_id"
+private const val INTEREST_TAG_TYPE = "tag_type"
+private const val CONFIG_TYPE = "config_type"
+private const val WRITING_CONTENT = "recruitment_content"
+
 fun log(event: String, parameters: ParametersBuilder.() -> Unit = {}) {
     KerdyApplication.firebaseAnalytics.logEvent(event, parameters)
 }
@@ -12,5 +34,49 @@ fun log(event: String, parameters: ParametersBuilder.() -> Unit = {}) {
 fun logScreen(screenName: String) {
     log(FirebaseAnalytics.Event.SCREEN_VIEW) {
         param(FirebaseAnalytics.Param.SCREEN_NAME, screenName)
+    }
+}
+
+fun logEventClick(eventName: String, eventId: Long, memberId: Long) {
+    log(EVENT_CLICK) {
+        param(EVENT_NAME, eventName)
+        param(EVENT_ID, eventId)
+        param(USER_ID, memberId)
+    }
+}
+
+fun logComment(commentName: String, commentId: Long, memberId: Long) {
+    log(COMMENT) {
+        param(EVENT_NAME, commentName)
+        param(EVENT_ID, commentId)
+        param(USER_ID, memberId)
+    }
+}
+
+fun logWriting(writingContent: String, memberId: Long) {
+    log(WRITE_RECRUITMENT) {
+        param(WRITING_CONTENT, writingContent)
+        param(USER_ID, memberId)
+    }
+}
+
+fun logRecruitment(recruitmentMessage: String, memberId: Long) {
+    log(RECRUITMENT) {
+        param(WRITING_CONTENT, recruitmentMessage)
+        param(USER_ID, memberId)
+    }
+}
+
+fun logInterestTags(tags: List<String>) {
+    tags.forEach { tag ->
+        log(INTEREST_TAGS) {
+            param(INTEREST_TAG_TYPE, tag)
+        }
+    }
+}
+
+fun logChangeConfig(configName: String) {
+    log(CHANGE_CONFIG) {
+        param(CONFIG_TYPE, configName)
     }
 }
