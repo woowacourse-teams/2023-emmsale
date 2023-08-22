8583fdd898e13ba93c372a1f7cd232d73ee191a5
buna <tmdgh1592@naver.com>
2023-08-22 12:15:38 +0900

    feat: ServiceFactory 코드 정렬

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/common/ServiceFactory.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/common/ServiceFactory.kt
@@ -1,5 +1,6 @@
 package com.emmsale.data.common
 
+import com.emmsale.BuildConfig
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -9,28 +10,35 @@ import retrofit2.Retrofit
 import java.util.concurrent.TimeUnit
 
 class ServiceFactory {
-    private val jsonMediaType = "application/json".toMediaType()
     private val json = Json {
         coerceInputValues = true
         encodeDefaults = true
         isLenient = true
     }
+    private val jsonMediaType = "application/json".toMediaType()
     private val jsonConverterFactory = json.asConverterFactory(jsonMediaType)
+
     private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
-    private val okhttpClient = OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor)
-        .addInterceptor(AuthInterceptor()).connectTimeout(120, TimeUnit.SECONDS)
-        .readTimeout(120, TimeUnit.SECONDS).writeTimeout(120, TimeUnit.SECONDS).build()
 
-    private val retrofit: Retrofit =
-        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(jsonConverterFactory)
-            .client(okhttpClient).build()
+    private val okhttpClient = OkHttpClient.Builder()
+        .addInterceptor(httpLoggingInterceptor)
+        .addInterceptor(AuthInterceptor())
+        .connectTimeout(120, TimeUnit.SECONDS)
+        .readTimeout(120, TimeUnit.SECONDS)
+        .writeTimeout(120, TimeUnit.SECONDS)
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(jsonConverterFactory)
+        .client(okhttpClient)
+        .build()
 
     fun <T> create(service: Class<T>): T = retrofit.create(service)
 
     companion object {
-        //        private const val BASE_URL = BuildConfig.BASE_URL
-        private const val BASE_URL = "https://prod.kerdy.kro.kr"
+        private const val BASE_URL = BuildConfig.BASE_URL
     }
 }
