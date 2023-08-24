f155166024595e992fee8c037ba13fa52980d2b2
ki960213 <ki960213@gmail.com>
2023-08-24 17:49:43 +0900

    refactor(primaryNotification): 주요 알림 화면 리팩터링
    - 알림 목록을 보여주는 기능은 아직 구현하지 않음

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/notificationBox/primaryNotification/PrimaryNotificationFragment.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/notificationBox/primaryNotification/PrimaryNotificationFragment.kt
@@ -6,69 +6,22 @@ import androidx.fragment.app.viewModels
 import com.emmsale.R
 import com.emmsale.databinding.FragmentPrimaryNotificationBinding
 import com.emmsale.presentation.base.BaseFragment
-import com.emmsale.presentation.common.extension.showSnackBar
 import com.emmsale.presentation.common.views.WarningDialog
-import com.emmsale.presentation.ui.eventdetail.EventDetailActivity
-import com.emmsale.presentation.ui.eventdetail.comment.childComment.ChildCommentActivity
-import com.emmsale.presentation.ui.notificationBox.primaryNotification.uistate.ChildCommentNotificationUiState
-import com.emmsale.presentation.ui.notificationBox.primaryNotification.uistate.InterestEventNotificationUiState
-import com.emmsale.presentation.ui.notificationBox.primaryNotification.uistate.PrimaryNotificationUiState
-import com.emmsale.presentation.ui.notificationBox.primaryNotification.uistate.PrimaryNotificationsUiEvent
 
 class PrimaryNotificationFragment : BaseFragment<FragmentPrimaryNotificationBinding>() {
     override val layoutResId: Int = R.layout.fragment_primary_notification
-    private val viewModel: PrimaryNotificationViewModel by viewModels {
-        PrimaryNotificationViewModel.factory
+    private val viewModel: PrimaryNotificationViewModel1 by viewModels {
+        PrimaryNotificationViewModel1.factory
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initView()
-        setupObservers()
+        initDataBinding()
     }
 
-    private fun initView() {
+    private fun initDataBinding() {
         binding.viewModel = viewModel
-    }
-
-    private fun setupObservers() {
-        setupUiEvent()
-    }
-
-    private fun setupUiEvent() {
-        viewModel.event.observe(viewLifecycleOwner) {
-            handleEvent(it)
-        }
-    }
-
-    private fun handleEvent(event: PrimaryNotificationsUiEvent?) {
-        if (event == null) return
-        when (event) {
-            PrimaryNotificationsUiEvent.DELETE_ERROR -> showNotificationDeleteConfirmDialog()
-        }
-        viewModel.resetEvent()
-    }
-
-    private fun showNotificationDeleteFailedMessage() {
-        binding.root.showSnackBar(R.string.primarynotification_delete_notification_failed_message)
-    }
-
-    private fun navigateToDetail(notification: PrimaryNotificationUiState) {
-        when (notification) {
-            is InterestEventNotificationUiState -> navigateToEventScreen(notification.eventId)
-            is ChildCommentNotificationUiState -> navigateToCommentScreen(
-                eventId = notification.eventId,
-                parentCommentId = notification.parentCommentId,
-            )
-        }
-    }
-
-    private fun navigateToEventScreen(eventId: Long) {
-        EventDetailActivity.startActivity(requireContext(), eventId)
-    }
-
-    private fun navigateToCommentScreen(eventId: Long, parentCommentId: Long) {
-        ChildCommentActivity.startActivity(requireContext(), eventId, parentCommentId)
+        binding.deleteAllNotifications = ::showNotificationDeleteConfirmDialog
     }
 
     private fun showNotificationDeleteConfirmDialog() {
