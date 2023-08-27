987c993f8d63ba6cb5d912ebce7161569fa84b76
chws0508 <chws0508@naver.com>
2023-08-28 00:33:05 +0900

    refactor: JUnit4를 안드로이드 테스트 라이브러리로 수정, Gradle Dependency 정리

--- android/2023-emmsale/app/build.gradle.kts
+++ android/2023-emmsale/app/build.gradle.kts
@@ -59,7 +59,6 @@ android {
         enable = true
     }
     tasks.withType(Test::class) {
-        useJUnitPlatform()
         testLogging {
             events.addAll(
                 arrayOf(
@@ -70,6 +69,9 @@ android {
             )
         }
     }
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
 }
 
 fun getApiKey(propertyKey: String): String {
@@ -77,39 +79,67 @@ fun getApiKey(propertyKey: String): String {
 }
 
 dependencies {
+
+    // Android
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.browser:browser:1.5.0")
     implementation("androidx.work:work-runtime-ktx:2.8.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
+
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+
+    // Kotlinx-Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+
+    // OkHttp3
     implementation("com.squareup.okhttp3:logging-interceptor:4.8.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+
+    // Glide
     implementation("com.github.bumptech.glide:glide:4.15.1")
+
+    // SwipeRefresh
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
+    // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-messaging-ktx")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
 
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
-    testImplementation("org.assertj", "assertj-core", "3.22.0")
     testImplementation("io.mockk:mockk-android:1.13.5")
     testImplementation("io.mockk:mockk-agent:1.13.5")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
+    // junit4
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.test.ext:junit:1.1.5")
+    testImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
+    // assertJ
+    testImplementation("org.assertj:assertj-core:3.22.0")
+
+    // android-test
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+
+    // recyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.1")
+
+    // SplashScreen
     implementation("androidx.core:core-splashscreen:1.0.0-beta01")
 
     // imageview
