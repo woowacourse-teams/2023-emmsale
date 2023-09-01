72b557a72433eac77eef702d07ef64dae51bc69c
amaran-th <songsy405@gmail.com>
2023-09-01 13:17:14 +0900

    feat: admin 페이지 연동을 위한 CORS 관련 설정 추가
    #145

---
+++ backend/emm-sale/src/main/java/com/emmsale/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.emmsale.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedMethods("*")
+        .exposedHeaders(HttpHeaders.LOCATION)
+        .allowedHeaders("*");
+  }
+}
