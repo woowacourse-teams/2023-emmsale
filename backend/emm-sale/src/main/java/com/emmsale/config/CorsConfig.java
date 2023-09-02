96afe802c0c67d410bd3db02b83ae296560db9ec
amaran-th <songsy405@gmail.com>
2023-09-02 12:05:53 +0900

    chore: CORS 설정에서 admin url 제한
     - yml 파일에 admin-url 데이터 추가
    
    #145

--- backend/emm-sale/src/main/java/com/emmsale/config/CorsConfig.java
+++ backend/emm-sale/src/main/java/com/emmsale/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.emmsale.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,9 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+  @Value("${data.admin-url}")
+  private String adminUrl;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
+        .allowedOrigins(adminUrl)
         .allowedMethods("*")
         .exposedHeaders(HttpHeaders.LOCATION)
         .allowedHeaders("*");
