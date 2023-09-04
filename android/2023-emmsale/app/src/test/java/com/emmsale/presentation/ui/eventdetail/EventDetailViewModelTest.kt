28a3782206998b4e937c11389ddd8d56c98cb566
Choi Woo Seok <chws0508@naver.com>
2023-09-04 15:31:58 +0900

    [Feat] EventInfoViewModel 테스트 구현 (#509)
    * refactor: JUnit4를 안드로이드 테스트 라이브러리로 수정, Gradle Dependency 정리
    
    * feat: EventDetailViewModelTest 구현
    
    * feat: EventInfoViewModelTest 구현

---
+++ android/2023-emmsale/app/src/test/java/com/emmsale/presentation/ui/eventdetail/EventDetailViewModelTest.kt
@@ -0,0 +1,102 @@
+package com.emmsale.presentation.ui.eventdetail
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.emmsale.data.common.ApiError
+import com.emmsale.data.common.ApiSuccess
+import com.emmsale.data.eventdetail.EventDetail
+import com.emmsale.data.eventdetail.EventDetailRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import okhttp3.Headers
+import org.assertj.core.api.SoftAssertions
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.time.LocalDateTime
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class EventDetailViewModelTest {
+
+    private lateinit var vm: EventDetailViewModel
+    private lateinit var eventDetailRepository: EventDetailRepository
+
+    private val testEventDetail = EventDetail(
+        id = 1L,
+        name = "Florence Joseph",
+        informationUrl = "https://search.yahoo.com/search?p=mediocrem",
+        startDate = LocalDateTime.now(),
+        endDate = LocalDateTime.now(),
+        applyStartDate = LocalDateTime.now(),
+        applyEndDate = LocalDateTime.now(),
+        location = "hinc",
+        status = "tamquam",
+        applyStatus = "graecis",
+        tags = listOf(),
+        posterImageUrl = null,
+        remainingDays = 9431,
+        applyRemainingDays = 2123,
+        type = "minim",
+    )
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        eventDetailRepository = mockk(relaxed = true)
+        vm = EventDetailViewModel(1L, eventDetailRepository)
+    }
+
+    @After
+    fun finish() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `정상적으로 상세정보를 받아온다면, id가 1인 상세정보를 요청했을 때, eventDetail 은 Error와 Loading 상태가 false 이고 id가 1이다 `() {
+        // given
+        coEvery {
+            eventDetailRepository.getEventDetail(1L)
+        } answers {
+            ApiSuccess(testEventDetail, Headers.headersOf("Auth", "hi"))
+        }
+
+        // when
+        vm.refresh()
+
+        // then
+        val softly = SoftAssertions().apply {
+            assertThat(vm.eventDetail.value.isError).isEqualTo(false)
+            assertThat(vm.eventDetail.value.isLoading).isEqualTo(false)
+            assertThat(vm.eventDetail.value.id).isEqualTo(1L)
+        }
+        softly.assertAll()
+    }
+
+    @Test
+    fun `정상적으로 상세정보를 받아올 수 없다면, id가 1인 상세정보를 요청했을 때, eventDetail은 Error 상태가 된다 `() {
+        // given
+        coEvery {
+            eventDetailRepository.getEventDetail(1L)
+        } answers {
+            ApiError(code = 4548, message = null)
+        }
+
+        // when
+        vm.refresh()
+
+        // then
+        val softly = SoftAssertions().apply {
+            assertThat(vm.eventDetail.value.isError).isEqualTo(true)
+            assertThat(vm.eventDetail.value.isLoading).isEqualTo(false)
+        }
+        softly.assertAll()
+    }
+}
