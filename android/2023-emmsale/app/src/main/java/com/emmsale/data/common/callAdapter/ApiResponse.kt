eefc67bd9a929bd032a6a342bc1d82dff4c41420
buna <tmdgh1592@naver.com>
2023-09-11 19:50:57 +0900

    feat: ApiResult에서 DTO -> DataModel로 변환하는 로직 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/common/callAdapter/ApiResponse.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/common/callAdapter/ApiResponse.kt
@@ -1,6 +1,14 @@
 package com.emmsale.data.common.callAdapter
 
-sealed class ApiResponse<out T : Any>
+sealed class ApiResponse<out T : Any> {
+    fun <R : Any> map(transform: (T) -> R): ApiResponse<R> = when (this) {
+        is Success -> Success(transform(data))
+        is Failure -> Failure(responseCode, message)
+        is NetworkError -> NetworkError
+        is Unexpected -> Unexpected(error)
+    }
+}
+
 data class Success<T : Any>(val data: T) : ApiResponse<T>()
 data class Failure(val responseCode: Int, val message: String?) : ApiResponse<Nothing>()
 object NetworkError : ApiResponse<Nothing>()
