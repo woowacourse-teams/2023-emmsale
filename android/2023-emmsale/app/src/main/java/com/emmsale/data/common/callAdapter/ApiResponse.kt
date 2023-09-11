8672e154c76ac01dd69084c404975c57c8eadaea
buna <tmdgh1592@naver.com>
2023-09-11 20:47:04 +0900

    feat: ApiResonse.Success에 Headers 프로퍼티 추가

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/common/callAdapter/ApiResponse.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/common/callAdapter/ApiResponse.kt
@@ -1,15 +1,18 @@
 package com.emmsale.data.common.callAdapter
 
+import okhttp3.Headers
+import okhttp3.internal.EMPTY_HEADERS
+
 sealed class ApiResponse<out T : Any> {
     fun <R : Any> map(transform: (T) -> R): ApiResponse<R> = when (this) {
-        is Success -> Success(transform(data))
+        is Success -> Success(transform(data), headers)
         is Failure -> Failure(code, message)
         is NetworkError -> NetworkError
         is Unexpected -> Unexpected(error)
     }
 }
 
-data class Success<T : Any>(val data: T) : ApiResponse<T>()
+data class Success<T : Any>(val data: T, val headers: Headers = EMPTY_HEADERS) : ApiResponse<T>()
 data class Failure(val code: Int, val message: String?) : ApiResponse<Nothing>()
 object NetworkError : ApiResponse<Nothing>()
 data class Unexpected(val error: Throwable?) : ApiResponse<Nothing>()
