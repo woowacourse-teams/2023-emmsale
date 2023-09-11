69bfa82f6ec1a29c747911944956182d08f15365
buna <tmdgh1592@naver.com>
2023-09-11 20:45:04 +0900

    refactor: ApiResponse.Failure의 responseCode를 code로 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/common/callAdapter/ApiResponse.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/common/callAdapter/ApiResponse.kt
@@ -3,13 +3,13 @@ package com.emmsale.data.common.callAdapter
 sealed class ApiResponse<out T : Any> {
     fun <R : Any> map(transform: (T) -> R): ApiResponse<R> = when (this) {
         is Success -> Success(transform(data))
-        is Failure -> Failure(responseCode, message)
+        is Failure -> Failure(code, message)
         is NetworkError -> NetworkError
         is Unexpected -> Unexpected(error)
     }
 }
 
 data class Success<T : Any>(val data: T) : ApiResponse<T>()
-data class Failure(val responseCode: Int, val message: String?) : ApiResponse<Nothing>()
+data class Failure(val code: Int, val message: String?) : ApiResponse<Nothing>()
 object NetworkError : ApiResponse<Nothing>()
 data class Unexpected(val error: Throwable?) : ApiResponse<Nothing>()
