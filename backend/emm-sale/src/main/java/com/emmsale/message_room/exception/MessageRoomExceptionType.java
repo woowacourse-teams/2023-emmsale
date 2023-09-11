633dc6971d25cd8929f1232e7fee14b3f944d1e8
java-saeng <gusdn85554@naver.com>
2023-09-12 03:29:11 +0900

    feat : 쪽지 관련 Exception 추가
    #528

---
+++ backend/emm-sale/src/main/java/com/emmsale/message_room/exception/MessageRoomExceptionType.java
@@ -0,0 +1,37 @@
+package com.emmsale.message_room.exception;
+
+import com.emmsale.base.BaseExceptionType;
+import org.springframework.http.HttpStatus;
+
+public enum MessageRoomExceptionType implements BaseExceptionType {
+
+  NOT_FOUND_MESSAGE_ROOM(
+      HttpStatus.NOT_FOUND,
+      "해당 쪽지방은 존재하지 않습니다."
+  ),
+
+  FORBIDDEN_NOT_INTERLOCUTORS(
+      HttpStatus.FORBIDDEN,
+      "해당 쪽지방에 참여하지 않았습니다."
+  )
+
+  ;
+
+  private final HttpStatus httpStatus;
+  private final String errorMessage;
+
+  MessageRoomExceptionType(final HttpStatus httpStatus, final String errorMessage) {
+    this.httpStatus = httpStatus;
+    this.errorMessage = errorMessage;
+  }
+
+  @Override
+  public HttpStatus httpStatus() {
+    return httpStatus;
+  }
+
+  @Override
+  public String errorMessage() {
+    return errorMessage;
+  }
+}
