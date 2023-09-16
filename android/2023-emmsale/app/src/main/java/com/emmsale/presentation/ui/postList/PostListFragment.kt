45b82e98fcc2607ff6ff93e5faeb1d7a0e3ddffb
chws0508 <chws0508@naver.com>
2023-09-16 22:18:10 +0900

    feat: 게시판 xml 구현

---
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/postList/PostListFragment.kt
@@ -0,0 +1,16 @@
+package com.emmsale.presentation.ui.postList
+
+import android.os.Bundle
+import android.view.View
+import com.emmsale.R
+import com.emmsale.databinding.FragmentPostListBinding
+import com.emmsale.presentation.base.BaseFragment
+
+class PostListFragment() : BaseFragment<FragmentPostListBinding>() {
+
+    override val layoutResId: Int = R.layout.fragment_post_list
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
