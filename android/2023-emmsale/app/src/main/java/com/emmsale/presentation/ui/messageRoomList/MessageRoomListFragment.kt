1d8d53c7be61c1bcbef5506e9ae8c5b4e27c414c
buna <tmdgh1592@naver.com>
2023-09-20 15:18:23 +0900

    refactor: MessageRoomList를 프래그먼트로 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/messageRoomList/MessageRoomListFragment.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/messageRoomList/MessageRoomListFragment.kt
@@ -1,28 +1,29 @@
 package com.emmsale.presentation.ui.messageRoomList
 
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.emmsale.R
 import com.emmsale.databinding.ActivityMessageRoomListBinding
+import com.emmsale.presentation.base.BaseFragment
 import com.emmsale.presentation.common.FetchResult
 import com.emmsale.presentation.ui.messageList.MessageListActivity
 import com.emmsale.presentation.ui.messageRoomList.recyclerview.MessageRoomListAdapter
 
-class MessageRoomListActivity : AppCompatActivity() {
-    private val binding by lazy { ActivityMessageRoomListBinding.inflate(layoutInflater) }
+class MessageRoomListFragment : BaseFragment<ActivityMessageRoomListBinding>() {
+    override val layoutResId: Int = R.layout.activity_message_room_list
     private val viewModel: MessageRoomListViewModel by viewModels { MessageRoomListViewModel.factory }
 
     private lateinit var messageRoomListAdapter: MessageRoomListAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         setupBinding()
         setupMessageRoomRecyclerView()
         setupMessageRooms()
     }
 
     private fun setupBinding() {
-        setContentView(binding.root)
         binding.lifecycleOwner = this
         binding.vm = viewModel
     }
@@ -33,13 +34,13 @@ class MessageRoomListActivity : AppCompatActivity() {
     }
 
     private fun setupMessageRooms() {
-        viewModel.messageRooms.observe(this) { uiState ->
+        viewModel.messageRooms.observe(viewLifecycleOwner) { uiState ->
             if (uiState.fetchResult != FetchResult.SUCCESS) return@observe
             messageRoomListAdapter.submitList(uiState.messageRooms)
         }
     }
 
     private fun navigateToMessageList(roomId: String, otherUid: Long) {
-        MessageListActivity.startActivity(this, roomId, otherUid)
+        MessageListActivity.startActivity(requireContext(), roomId, otherUid)
     }
 }
