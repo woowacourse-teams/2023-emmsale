826cca75e906dd21599b093bda282364755c6b33
chws0508 <chws0508@naver.com>
2023-09-21 15:57:32 +0900

    refactor : 스크랩 화면 행사 목록 화면과 동일하게 적용

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/model/ScrappedEvent.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/model/ScrappedEvent.kt
@@ -1,10 +1,30 @@
 package com.emmsale.data.model
 
+import java.time.LocalDateTime
+
 data class ScrappedEvent(
-    val scrapId: Long,
-    val eventId: Long,
+    val id: Long,
     val name: String,
-    val status: ConferenceStatus,
-    val imageUrl: String?,
-    val tags: List<String> = listOf(),
-)
+    val startDate: LocalDateTime,
+    val endDate: LocalDateTime,
+    val status: Status,
+    val tags: List<String>,
+    val posterUrl: String?,
+    val dDay: Int,
+    val eventStatus: Status,
+    val applyRemainingDays: Int,
+    val eventMode: EventMode,
+    val paymentType: PaymentType,
+) {
+    enum class Status {
+        IN_PROGRESS, UPCOMING, ENDED,
+    }
+
+    enum class EventMode {
+        ONLINE, OFFLINE, ON_OFFLINE,
+    }
+
+    enum class PaymentType {
+        PAID, FREE, PAID_OR_FREE,
+    }
+}
--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/scrappedEventList/uiState/ScrappedEventUiState.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/scrappedEventList/uiState/ScrappedEventUiState.kt
@@ -1,32 +1,20 @@
 package com.emmsale.presentation.ui.scrappedEventList.uiState
 
-import com.emmsale.data.model.ConferenceStatus
 import com.emmsale.data.model.ScrappedEvent
 
 data class ScrappedEventUiState(
-    val scrapId: Long = DEFAULT_ID,
-    val eventId: Long = DEFAULT_ID,
-    val name: String = "",
-    val status: String = "",
-    val imageUrl: String?,
-    val tags: List<String> = listOf(),
+    val data: ScrappedEvent,
 ) {
+
+    fun getStatusString(): String = when (data.status) {
+        ScrappedEvent.Status.IN_PROGRESS -> "진행중"
+        ScrappedEvent.Status.UPCOMING -> "D-${data.dDay}"
+        ScrappedEvent.Status.ENDED -> "마감"
+    }
+
     companion object {
-        private const val DEFAULT_ID = -1L
-
         fun from(scrappedEvent: ScrappedEvent) = ScrappedEventUiState(
-            scrappedEvent.scrapId,
-            scrappedEvent.eventId,
-            scrappedEvent.name,
-            getStatus(scrappedEvent),
-            scrappedEvent.imageUrl,
-            scrappedEvent.tags,
+            data = scrappedEvent,
         )
-
-        private fun getStatus(scrappedEvent: ScrappedEvent): String = when (scrappedEvent.status) {
-            ConferenceStatus.IN_PROGRESS -> "진행중"
-            ConferenceStatus.UPCOMING -> "진행 예정"
-            ConferenceStatus.ENDED -> "마감"
-        }
     }
 }
