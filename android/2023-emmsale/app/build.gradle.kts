ee673669abe4c786ff6b01d5d02a66a92f8a0ea7
ki960213 <ki960213@gmail.com>
2023-09-21 22:45:00 +0900

    chore: 버전 올림
    - 33 -> 34

--- android/2023-emmsale/app/build.gradle.kts
+++ android/2023-emmsale/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.emmsale"
         minSdk = 28
         targetSdk = 33
-        versionCode = 33
+        versionCode = 34
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
