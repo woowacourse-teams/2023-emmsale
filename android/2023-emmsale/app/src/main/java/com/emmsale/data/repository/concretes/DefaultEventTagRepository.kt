38c685a1cf2490371ba3e13e67eb4dafb0f4d3a3
토마스 <123928686+ki960213@users.noreply.github.com>
2023-09-21 23:35:32 +0900

    merge: 태그 알림 설정 화면 수정 (#614)
    Related to: #613

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/repository/concretes/DefaultEventTagRepository.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/repository/concretes/DefaultEventTagRepository.kt
@@ -3,7 +3,6 @@ package com.emmsale.data.repository.concretes
 import com.emmsale.data.apiModel.request.InterestEventTagUpdateRequest
 import com.emmsale.data.apiModel.response.EventTagResponse
 import com.emmsale.data.common.callAdapter.ApiResponse
-import com.emmsale.data.common.callAdapter.Success
 import com.emmsale.data.dataSource.remote.EventTagRemoteDataSource
 import com.emmsale.data.mapper.toData
 import com.emmsale.data.model.EventTag
@@ -16,17 +15,11 @@ class DefaultEventTagRepository(
     private val eventTagRemoteDataSource: EventTagRemoteDataSource,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : EventTagRepository {
-    private val cachedEventTags = mutableListOf<EventTag>()
 
     override suspend fun getEventTags(): ApiResponse<List<EventTag>> = withContext(dispatcher) {
-        if (cachedEventTags.isNotEmpty()) {
-            return@withContext Success(cachedEventTags.map(EventTag::copy))
-        }
-
         eventTagRemoteDataSource
             .getEventTags()
             .map(List<EventTagResponse>::toData)
-            .apply { if (this is Success) cacheEventTags(data) }
     }
 
     override suspend fun getEventTagByIds(tagIds: Array<Long>): ApiResponse<List<EventTag>> =
@@ -39,37 +32,17 @@ class DefaultEventTagRepository(
                 .map(List<EventTagResponse>::toData)
         }
 
-    override suspend fun getInterestEventTags(memberId: Long): ApiResponse<List<EventTag>> {
-        if (cachedEventTags.isNotEmpty()) {
-            return Success(cachedEventTags.map(EventTag::copy))
-        }
-        val apiResponse = withContext(dispatcher) {
+    override suspend fun getInterestEventTags(memberId: Long): ApiResponse<List<EventTag>> =
+        withContext(dispatcher) {
             eventTagRemoteDataSource
                 .getInterestEventTags(memberId)
                 .map(List<EventTagResponse>::toData)
         }
 
-        if (apiResponse is Success) {
-            cacheEventTags(apiResponse.data)
-        }
-        return apiResponse
-    }
-
-    override suspend fun updateInterestEventTags(interestEventTags: List<EventTag>): ApiResponse<Unit> {
-        val apiResponse = withContext(dispatcher) {
+    override suspend fun updateInterestEventTags(interestEventTags: List<EventTag>): ApiResponse<Unit> =
+        withContext(dispatcher) {
             eventTagRemoteDataSource.updateInterestEventTags(
                 InterestEventTagUpdateRequest(interestEventTags.map { it.id }),
             ).map { }
         }
-
-        if (apiResponse is Success) {
-            cacheEventTags(interestEventTags)
-        }
-        return apiResponse
-    }
-
-    private fun cacheEventTags(interestEventTags: List<EventTag>) {
-        cachedEventTags.clear()
-        cachedEventTags.addAll(interestEventTags)
-    }
 }
