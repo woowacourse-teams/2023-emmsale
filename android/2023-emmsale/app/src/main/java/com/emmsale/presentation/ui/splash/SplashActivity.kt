2b6a016fcf53b56db6b930aed2fa4c613c227d53
ki960213 <ki960213@gmail.com>
2023-09-21 23:33:07 +0900

    temp: 스플래시 화면에서 업데이트 확인하도록 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/splash/SplashActivity.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/splash/SplashActivity.kt
@@ -41,8 +41,7 @@ class SplashActivity : AppCompatActivity() {
         viewModel.splash.observe(this) { splashState ->
             when (splashState) {
                 is SplashUiState.Loading -> initSplashAnimation(splashState.splashTimeMs)
-                // is SplashUiState.Done -> checkAppUpdate(splashState.isAutoLogin)
-                is SplashUiState.Done -> navigateToLoginScreen()
+                is SplashUiState.Done -> checkAppUpdate(splashState.isAutoLogin)
             }
         }
     }
