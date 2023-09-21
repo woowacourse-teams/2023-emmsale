460731346cbc774c394f65e295cf566448fd4dfc
ki960213 <ki960213@gmail.com>
2023-09-21 20:10:42 +0900

    style: ktlint 적용

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/login/LoginActivity.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/login/LoginActivity.kt
@@ -19,7 +19,6 @@ import com.emmsale.presentation.common.extension.showSnackBar
 import com.emmsale.presentation.common.extension.showToast
 import com.emmsale.presentation.common.firebase.analytics.FirebaseAnalyticsDelegate
 import com.emmsale.presentation.common.firebase.analytics.FirebaseAnalyticsDelegateImpl
-import com.emmsale.presentation.ui.feedDetail.FeedDetailActivity
 import com.emmsale.presentation.ui.login.uiState.LoginUiState
 import com.emmsale.presentation.ui.main.MainActivity
 import com.emmsale.presentation.ui.onboarding.OnboardingActivity
