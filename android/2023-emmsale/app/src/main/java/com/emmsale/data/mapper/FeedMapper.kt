c5bf43145dd5ec39a5f53af631a09c2e530fc22d
ki960213 <ki960213@gmail.com>
2023-09-21 19:19:54 +0900

    fix(FeedMapper): 이미지 url에 prefix 붙이기

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/mapper/FeedMapper.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/mapper/FeedMapper.kt
@@ -1,6 +1,7 @@
 package com.emmsale.data.mapper
 
 import com.emmsale.data.apiModel.response.FeedDetailResponse
+import com.emmsale.data.common.imagePrefixUrl
 import com.emmsale.data.model.FeedDetail
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -11,7 +12,7 @@ fun FeedDetailResponse.toData(): FeedDetail = FeedDetail(
     title = title,
     content = content,
     writer = writer.toData(),
-    imageUrls = imageUrls,
+    imageUrls = imageUrls.map { imagePrefixUrl + it },
     createdAt = LocalDateTime.parse(createdAt, dateTimeFormatter),
     updatedAt = LocalDateTime.parse(updatedAt, dateTimeFormatter),
 )
