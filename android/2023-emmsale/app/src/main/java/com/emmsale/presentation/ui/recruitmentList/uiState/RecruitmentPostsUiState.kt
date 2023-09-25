8f8886e0c9e29cda7d50305740c2fd9575b8e3cd
buna <tmdgh1592@naver.com>
2023-09-25 18:19:13 +0900

    feat: 게시물이 없을 때의 화면을 보여주는 기능 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/recruitmentList/uiState/RecruitmentPostsUiState.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/recruitmentList/uiState/RecruitmentPostsUiState.kt
@@ -1,28 +1,26 @@
 package com.emmsale.presentation.ui.recruitmentList.uiState
 
 import com.emmsale.data.model.Recruitment
+import com.emmsale.presentation.common.FetchResult
+import com.emmsale.presentation.common.FetchResultUiState
 
 data class RecruitmentPostsUiState(
     val list: List<RecruitmentPostUiState> = listOf(),
-    val isLoading: Boolean = false,
-    val isError: Boolean = false,
-) {
+    override val fetchResult: FetchResult = FetchResult.LOADING,
+) : FetchResultUiState() {
     fun changeToLoadingState() = copy(
-        isLoading = true,
-        isError = false,
+        fetchResult = FetchResult.LOADING,
     )
 
     fun changeToErrorState() = copy(
-        isLoading = false,
-        isError = true,
+        fetchResult = FetchResult.ERROR,
     )
 
     companion object {
         fun from(recruitments: List<Recruitment>): RecruitmentPostsUiState {
             return RecruitmentPostsUiState(
                 list = recruitments.map { RecruitmentPostUiState.from(it) },
-                isLoading = false,
-                isError = false,
+                fetchResult = FetchResult.SUCCESS,
             )
         }
     }
