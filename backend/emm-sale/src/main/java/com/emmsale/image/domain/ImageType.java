fc31ef3be1900991100c83c92a695d0cccd506b2
amaran-th <songsy405@naver.com>
2023-09-25 15:51:56 +0900

    refactor: 이미지 상한이 없는 컨텐츠의 maxImageCount 값을 0으로 설정
    #633

--- backend/emm-sale/src/main/java/com/emmsale/image/domain/ImageType.java
+++ backend/emm-sale/src/main/java/com/emmsale/image/domain/ImageType.java
@@ -2,8 +2,9 @@ package com.emmsale.image.domain;
 
 public enum ImageType {
   FEED(5),
-  EVENT(200);
+  EVENT(0);
 
+  private static final int NO_LIMIT_COUNT = 0;
   private final int maxImageCount;
 
   ImageType(final int maxImageCount) {
@@ -11,6 +12,9 @@ public enum ImageType {
   }
 
   public boolean isOverMaxImageCount(final int imageCount) {
+    if (maxImageCount == NO_LIMIT_COUNT) {
+      return false;
+    }
     return imageCount > maxImageCount;
   }
 
