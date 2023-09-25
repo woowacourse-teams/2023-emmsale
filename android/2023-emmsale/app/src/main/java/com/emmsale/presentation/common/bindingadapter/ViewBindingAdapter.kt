cc1a3a758b86796089d9c7e27db71bdde88d6483
buna <tmdgh1592@naver.com>
2023-09-25 14:55:46 +0900

    feat: 스크랩 화면이 비어있을 때 보여주는 화면 구현

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/bindingadapter/ViewBindingAdapter.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/bindingadapter/ViewBindingAdapter.kt
@@ -5,6 +5,11 @@ import android.view.ViewGroup
 import androidx.core.view.updateLayoutParams
 import androidx.databinding.BindingAdapter
 
+@BindingAdapter("app:visible")
+fun View.setVisible(visible: Boolean) {
+    visibility = if (visible) View.VISIBLE else View.GONE
+}
+
 @BindingAdapter("app:layoutMarginTop")
 fun View.setLayoutMarginTop(dimen: Float) {
     updateLayoutParams<ViewGroup.MarginLayoutParams> {
