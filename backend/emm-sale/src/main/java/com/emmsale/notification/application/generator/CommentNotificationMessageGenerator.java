24b4dce9c4f22431bf0238da215d0281a6cf94a6
java-saeng <gusdn85554@naver.com>
2023-09-27 13:24:05 +0900

    feat : 댓글 알림 Message Generator, Message 구현
    #649

---
+++ backend/emm-sale/src/main/java/com/emmsale/notification/application/generator/CommentNotificationMessageGenerator.java
@@ -0,0 +1,55 @@
+package com.emmsale.notification.application.generator;
+
+import static com.emmsale.notification.exception.NotificationExceptionType.BAD_REQUEST_MEMBER_ID;
+import static com.emmsale.notification.exception.NotificationExceptionType.CONVERTING_JSON_ERROR;
+
+import com.emmsale.member.domain.MemberRepository;
+import com.emmsale.notification.application.dto.CommentNotificationMessage;
+import com.emmsale.notification.application.dto.CommentNotificationMessage.Data;
+import com.emmsale.notification.application.dto.CommentNotificationMessage.Message;
+import com.emmsale.notification.domain.Notification;
+import com.emmsale.notification.exception.NotificationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@Slf4j
+public class CommentNotificationMessageGenerator implements NotificationMessageGenerator {
+
+  private final Notification notification;
+
+  @Override
+  public String makeMessage(
+      final String targetToken,
+      final ObjectMapper objectMapper,
+      final MemberRepository memberRepository
+  ) {
+
+    final String jsonData = notification.getJsonData();
+
+    try {
+
+      final Data data = objectMapper.readValue(jsonData, Data.class);
+
+      validateIsExistedReceiver(memberRepository, Long.valueOf(data.getReceiverId()));
+
+      final CommentNotificationMessage message = new CommentNotificationMessage(
+          DEFAULT_VALIDATE_ONLY, new Message(data, targetToken)
+      );
+
+      return objectMapper.writeValueAsString(message);
+
+    } catch (JsonProcessingException e) {
+      throw new NotificationException(CONVERTING_JSON_ERROR);
+    }
+  }
+
+  private void validateIsExistedReceiver(final MemberRepository memberRepository,
+      final Long receiverId) {
+    if (!memberRepository.existsById(receiverId)) {
+      throw new NotificationException(BAD_REQUEST_MEMBER_ID);
+    }
+  }
+}
