95da06b577dd9d02b25c8f0e9e36b8074e1c52d9
java-saeng <gusdn85554@naver.com>
2023-09-30 03:45:59 +0900

    feat : 알림 삭제 관한 메서드 추가
    #663

--- backend/emm-sale/src/main/java/com/emmsale/notification/domain/NotificationRepository.java
+++ backend/emm-sale/src/main/java/com/emmsale/notification/domain/NotificationRepository.java
@@ -2,6 +2,7 @@ package com.emmsale.notification.domain;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -9,4 +10,10 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
 
   @Query("select n from Notification n where n.receiverId = :receiverId")
   List<Notification> findAllByReceiverId(@Param("receiverId") final Long receiverId);
+
+  List<Notification> findAllByIdIn(final List<Long> notificationIds);
+
+  @Modifying
+  @Query("delete from Notification n where n.id in :ids")
+  void deleteBatchByIdsIn(@Param("ids") final List<Long> notificationIds);
 }
--- backend/emm-sale/src/test/java/com/emmsale/notification/domain/NotificationRepositoryTest.java
+++ backend/emm-sale/src/test/java/com/emmsale/notification/domain/NotificationRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.emmsale.notification.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.emmsale.helper.JpaRepositorySliceTestHelper;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,47 +18,47 @@ class NotificationRepositoryTest extends JpaRepositorySliceTestHelper {
   @Autowired
   private NotificationRepository notificationRepository;
 
-  private String commentJsonData1, commentJsonData2, eventJsonData1;
+  private Notification notification1, notification2, notification3;
+  private Long receiverId = 26L;
 
   @BeforeEach
   void setUp() {
-    commentJsonData1 = "{"
+    final String commentJsonData1 = "{"
         + "\"content\":\"content\","
         + "\"writer\":\"writer\","
         + "\"writerImageUrl\":\"imageUrl\""
         + "}";
 
-    eventJsonData1 = "{"
+    final String eventJsonData1 = "{"
         + "\"title\":\"title\""
         + "}";
 
-    commentJsonData2 = "{"
+    final String commentJsonData2 = "{"
         + "\"content\":\"content\","
         + "\"writer\":\"writer\","
         + "\"writerImageUrl\":\"imageUrl\""
         + "}";
+
+    notification1 = notificationRepository.save(
+        new Notification(NotificationType.COMMENT, receiverId, 3333L, LocalDateTime.now(),
+            commentJsonData1)
+    );
+
+    notification2 = notificationRepository.save(
+        new Notification(NotificationType.EVENT, receiverId, 3333L, LocalDateTime.now(),
+            eventJsonData1)
+    );
+
+    notification3 = notificationRepository.save(
+        new Notification(NotificationType.COMMENT, receiverId, 3333L, LocalDateTime.now(),
+            commentJsonData2)
+    );
   }
 
   @Test
   @DisplayName("findAllByReceiverId() : 특정 사용자가 받은 알림 목록들을 조회할 수 있다.")
   void test_findAllByReceiverId() throws Exception {
     //given
-    final long receiverId = 26L;
-
-    final Notification notification1 = notificationRepository.save(
-        new Notification(NotificationType.COMMENT, receiverId, 3333L, LocalDateTime.now(),
-            commentJsonData1)
-    );
-
-    final Notification notification2 = notificationRepository.save(
-        new Notification(NotificationType.EVENT, receiverId, 3333L, LocalDateTime.now(),
-            eventJsonData1)
-    );
-
-    notificationRepository.save(
-        new Notification(NotificationType.COMMENT, receiverId, 3333L, LocalDateTime.now(),
-            commentJsonData2));
-
     final List<Notification> expect = List.of(notification1, notification2);
 
     //when
@@ -64,5 +68,51 @@ class NotificationRepositoryTest extends JpaRepositorySliceTestHelper {
     Assertions.assertThat(actual)
         .usingRecursiveComparison()
         .isEqualTo(expect);
+  }
+
+  @Test
+  @DisplayName("findAllByIdIn() : 해당 id들을 가진 Notification들을 조회할 수 있다.")
+  void test_findAllByIdsIn() throws Exception {
+    //given
+    final List<Notification> expected = List.of(notification1, notification2);
+
+    final List<Long> notificationIds = List.of(notification1.getId(), notification2.getId());
+
+    //when
+    final List<Notification> actual =
+        notificationRepository.findAllByIdIn(notificationIds);
+
+    //then
+    assertThat(actual)
+        .usingRecursiveComparison()
+        .ignoringCollectionOrder()
+        .ignoringFields("createdAt")
+        .isEqualTo(expected);
+  }
+
+  @Test
+  @DisplayName("deleteBatchByIdsIn() : IN 쿼리를 통해 알림들을 배치 삭제할 수 있다.")
+  void test_deleteBatchByIdsIn() throws Exception {
+    //given
+    //존재하지 않는 알림 ID
+    final List<Long> nonExistedNotificationIds = List.of(3L, 4L, 5L);
+    final List<Long> existedNotificationIds = List.of(notification1.getId(), notification2.getId());
+    final List<Long> deleteIds = new ArrayList<>();
+    deleteIds.addAll(nonExistedNotificationIds);
+    deleteIds.addAll(existedNotificationIds);
+
+    final List<Long> actual = Collections.emptyList();
+
+    //when
+    notificationRepository.deleteBatchByIdsIn(deleteIds);
+
+    //then
+    final List<Notification> expected = notificationRepository.findAllByIdIn(deleteIds);
+
+    assertThat(actual)
+        .usingRecursiveComparison()
+        .ignoringCollectionOrder()
+        .ignoringFields("createdAt")
+        .isEqualTo(expected);
   }
 }
