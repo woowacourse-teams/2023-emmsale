c0a64b3ebe42783e61a06435839a8220eebad042
java-saeng <gusdn85554@naver.com>
2023-10-05 13:45:14 +0900

    refactor : @Transactional readOnly 추가
    #663

--- backend/emm-sale/src/main/java/com/emmsale/notification/application/NotificationQueryService.java
+++ backend/emm-sale/src/main/java/com/emmsale/notification/application/NotificationQueryService.java
@@ -10,9 +10,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class NotificationQueryService {
 
   private final NotificationRepository notificationRepository;
