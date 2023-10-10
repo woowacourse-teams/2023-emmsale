9bbfe91c114eea2fadc9fb7ba99a6b4446c05023
hong-sile <gurwns9325@naver.com>
2023-10-10 15:40:51 +0900

    fix: mutlipart에 RequestPart 추가
    #702

--- backend/emm-sale/src/main/java/com/emmsale/member/api/MemberApi.java
+++ backend/emm-sale/src/main/java/com/emmsale/member/api/MemberApi.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -101,7 +102,7 @@ public class MemberApi {
   @PatchMapping("/members/{memberId}/profile")
   public ResponseEntity<String> updateProfile(
       @PathVariable final Long memberId,
-      final MultipartFile image,
+      @RequestPart final MultipartFile image,
       final Member member
   ) {
     final String imageUrl = memberUpdateService.updateMemberProfile(image, memberId, member);
