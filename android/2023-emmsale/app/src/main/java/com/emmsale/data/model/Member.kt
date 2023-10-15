d0fb43d31fe048ec9bd432e6243833f07bae750c
chws0508 <chws0508@naver.com>
2023-10-15 20:20:07 +0900

    refactor : Member 모델에 기본값 부여

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/model/Member.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/model/Member.kt
@@ -2,9 +2,9 @@ package com.emmsale.data.model
 
 data class Member(
     val id: Long,
-    val githubUrl: String,
-    val name: String,
-    val description: String,
-    val profileImageUrl: String,
+    val githubUrl: String = "",
+    val name: String = "",
+    val description: String = "",
+    val profileImageUrl: String = "",
     val activities: List<Activity> = emptyList(),
 )
