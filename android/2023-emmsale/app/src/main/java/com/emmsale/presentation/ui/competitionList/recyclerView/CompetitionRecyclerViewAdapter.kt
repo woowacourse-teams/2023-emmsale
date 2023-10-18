4a49562233e559254a51d9d770643fc9a5e7fd46
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-10-18 15:24:37 +0900

    Refactor/#732 이미지 불러오는 속도 개선 (#738)
    * refactor: 오픈 채팅방 URL 설정 Activity를 manifest에서 제거
    
    - 사용되지 않음
    
    * refactor: 컨퍼런스 이미지를 8개까지 미리 불러와 캐싱하도록 변경
    
    * refactor: 대회 이미지를 8개까지 미리 불러와 캐싱하도록 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/competitionList/recyclerView/CompetitionRecyclerViewAdapter.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/competitionList/recyclerView/CompetitionRecyclerViewAdapter.kt
@@ -1,7 +1,9 @@
 package com.emmsale.presentation.ui.competitionList.recyclerView
 
+import android.content.Context
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
+import com.bumptech.glide.Glide
 import com.emmsale.data.model.Event
 import com.emmsale.presentation.ui.conferenceList.recyclerView.EventDiffUtil
 
@@ -15,5 +17,24 @@ class CompetitionRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: CompetitionViewHolder, position: Int) {
         holder.bind(getItem(position))
+        preload(holder.itemView.context, position)
+    }
+
+    private fun preload(context: Context, currentPosition: Int) {
+        val endPosition = (currentPosition + PRELOAD_SIZE).coerceAtMost(currentList.size - 1)
+
+        currentList
+            .subList(currentPosition, endPosition)
+            .forEach { event -> preload(context, event.posterUrl) }
+    }
+
+    private fun preload(context: Context, url: String?) {
+        Glide.with(context)
+            .load(url)
+            .preload()
+    }
+
+    companion object {
+        private const val PRELOAD_SIZE = 8
     }
 }
