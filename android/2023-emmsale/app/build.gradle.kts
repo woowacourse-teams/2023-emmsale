b6bda9d4ea104a182018a60233f866e1a88c75a4
chws0508 <chws0508@naver.com>
2023-10-19 18:02:09 +0900

    release:  2.1.0 버젼 재출시

--- android/2023-emmsale/app/build.gradle.kts
+++ android/2023-emmsale/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.emmsale"
         minSdk = 28
         targetSdk = 33
-        versionCode = 51
-        versionName = "2.0.2"
+        versionCode = 52
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
