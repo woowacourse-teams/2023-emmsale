7a176e3944e6c05037e6d104ce94febcadb281fb
chws0508 <chws0508@naver.com>
2023-10-25 12:02:30 +0900

    feat: ImageUtil 에 BitMap으로 변환 후, File로 변환하는 로직 추가, 이미지 권한 로직 추가

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/imageUtil/ImageUtil.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/common/imageUtil/ImageUtil.kt
@@ -1,10 +1,39 @@
 package com.emmsale.presentation.common.imageUtil
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.database.Cursor
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.FileOutputStream
+
+fun getFileFromUri(context: Context, uri: Uri): File {
+    val bitmap = uri.getBitmapFromUri(context)
+    val file = bitmap?.convertToFile(context)
+    return file ?: File("")
+}
+
+private fun Uri.getBitmapFromUri(context: Context): Bitmap? {
+    return context.contentResolver.openInputStream(this)?.use {
+        BitmapFactory.decodeStream(it)
+    }
+}
+
+private fun Bitmap.convertToFile(context: Context): File {
+    val tempFile = File.createTempFile("imageFile", ".jpg", context.cacheDir)
+
+    FileOutputStream(tempFile).use { fileOutputStream ->
+        this.compress(Bitmap.CompressFormat.JPEG, HIGHEST_COMPRESS_QUALITY, fileOutputStream)
+    }
+    return tempFile
+}
 
 fun Uri.convertToAbsolutePath(context: Context): String? {
     val projection = arrayOf(MediaStore.Images.Media.DATA)
@@ -22,5 +51,59 @@ fun Uri.convertToAbsolutePath(context: Context): String? {
     }
 }
 
+fun AppCompatActivity.checkImagePermission(
+    onGranted: () -> Unit,
+    onFirstDenied: (String) -> Unit,
+    onDenied: () -> Unit,
+) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        checkImagePermission(
+            Manifest.permission.READ_MEDIA_IMAGES,
+            onGranted,
+            onFirstDenied,
+            onDenied,
+        )
+    } else {
+        checkImagePermission(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            onGranted,
+            onFirstDenied,
+            onDenied,
+        )
+    }
+}
+
+fun AppCompatActivity.checkImagePermission(
+    permission: String,
+    onGranted: () -> Unit,
+    onFirstDenied: (String) -> Unit,
+    onDenied: () -> Unit,
+) {
+    when {
+        isImageAccessPermissionGranted() -> onGranted()
+
+        !isImageAccessPermissionGranted() &&
+            shouldShowRequestPermissionRationale(permission) -> onFirstDenied(permission)
+
+        else -> onDenied()
+    }
+}
+
+fun Context.isImageAccessPermissionGranted(): Boolean {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_MEDIA_IMAGES,
+        ) == PackageManager.PERMISSION_GRANTED
+    } else {
+        ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+}
+
 fun isPhotoPickerAvailable(): Boolean =
     Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+
+private const val HIGHEST_COMPRESS_QUALITY = 100
