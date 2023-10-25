47ca1e3537d6c054dc0dbb78a32e8112566179b8
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-10-25 14:57:17 +0900

    Feature/#771 채팅창 키보드 외 영역을 클릭하면 키보드가 닫히는 기능 (#791)
    * feat: 채팅창에서 키보드 바깥 영역을 터치하면 키보드 닫히는 기능 구현
    
    * refactor(MessageListActivity): 리사이클러뷰에 터치 이벤트를 넘겨주지 않고 액티비티에서 처리하도록 변경
    
    * feat(MessageListActivity): 스크롤하면 키보드가 닫히지 않고, 단순히 화면을 터치한 경우에만 키보드가 닫히도록 변경
    
    * refactor: 화면 터치시 키보드를 다루는 로직을 함수로 분리
    
    * fix: 미세하게 움직이면 키보드가 닫히지 않는 버그 수정
    
    * refactor(KeyboardHider): calculateHideState에서handleHideness로 이름 변경
    
    * refactor(KeyboardHider): canHideKeyboard를 메서드로 변경
    
    * refactor(KeyboardHider): isDeliverTouchEvent 프로퍼티명을 willConsumeTouchEvent로 변경
    
    * refactor(KeyboardHider): 터치 이벤트 소비를 의미하는 상수명 변경

---
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/messageList/KeyboardHider.kt
@@ -0,0 +1,44 @@
+package com.emmsale.presentation.ui.messageList
+
+import android.content.Context
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlin.math.abs
+
+class KeyboardHider(
+    private val targetView: View,
+    private val sensitivity: Float = DEFAULT_SENSITIVITY,
+    private val willConsumeTouchEvent: Boolean = CONSUMED_TOUCH_EVENT,
+) {
+    private val imm by lazy {
+        targetView.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
+
+    private var startY: Float = -1F
+    private var movedY: Float = 0F
+
+    fun handleHideness(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> startY = event.y
+            MotionEvent.ACTION_MOVE -> movedY = abs(event.y - startY)
+            MotionEvent.ACTION_UP -> if (canHideKeyboard()) hideKeyboard()
+        }
+        return willConsumeTouchEvent
+    }
+
+    private fun canHideKeyboard(): Boolean {
+        return movedY < sensitivity
+    }
+
+    private fun hideKeyboard() {
+        if (targetView.onCheckIsTextEditor()) {
+            imm.hideSoftInputFromWindow(targetView.windowToken, 0)
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_SENSITIVITY: Float = 10F
+        private const val CONSUMED_TOUCH_EVENT = false
+    }
+}
