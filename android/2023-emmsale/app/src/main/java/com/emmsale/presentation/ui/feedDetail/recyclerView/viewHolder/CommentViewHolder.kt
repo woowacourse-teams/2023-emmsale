884923fccbc5ddde2d3d9419927c879a0d6ac336
ki960213 <ki960213@gmail.com>
2023-10-30 02:05:12 +0900

    refactor(item_all_comment.xml): 파일명 변경
    - item_all_comment1.xml -> item_all_comment.xml

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/feedDetail/recyclerView/viewHolder/CommentViewHolder.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/feedDetail/recyclerView/viewHolder/CommentViewHolder.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.emmsale.R
 import com.emmsale.data.model.Comment
-import com.emmsale.databinding.ItemAllComment1Binding
+import com.emmsale.databinding.ItemAllCommentBinding
 import com.emmsale.presentation.ui.feedDetail.uiState.CommentUiState
 
 @SuppressLint("ClickableViewAccessibility")
@@ -16,9 +16,9 @@ class CommentViewHolder(
     onAuthorImageClick: (authorId: Long) -> Unit,
     onCommentMenuClick: (isWrittenByLoginUser: Boolean, commentId: Long) -> Unit,
 ) : RecyclerView.ViewHolder(
-    LayoutInflater.from(parent.context).inflate(R.layout.item_all_comment1, parent, false),
+    LayoutInflater.from(parent.context).inflate(R.layout.item_all_comment, parent, false),
 ) {
-    private val binding: ItemAllComment1Binding = ItemAllComment1Binding.bind(itemView)
+    private val binding: ItemAllCommentBinding = ItemAllCommentBinding.bind(itemView)
 
     init {
         binding.onClick = onClick
