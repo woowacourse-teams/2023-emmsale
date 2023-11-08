08f8f034facf6ce75f2986065c05e24f86a662e8
BuNa <56534241+tmdgh1592@users.noreply.github.com>
2023-11-08 16:17:40 +0900

    feat: 응답 코드가 401이면 로그인 화면으로 이동하는 기능 구현 (#831)
    * feat: 응답 코드가 401이면 로그인 화면으로 이동하는 기능 구현
    
    * feat: 토큰이 만료되었을 때 토큰을 제거하는 로직 제거
    
    * refactor: isAccessTokenInvalid로 메서드명 변경

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/common/retrofit/AuthInterceptor.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/common/retrofit/AuthInterceptor.kt
@@ -1,25 +1,47 @@
 package com.emmsale.data.common.retrofit
 
 import android.content.Context
+import android.content.Intent
 import com.emmsale.data.repository.interfaces.TokenRepository
+import com.emmsale.presentation.ui.login.LoginActivity
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.components.SingletonComponent
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 
-class AuthInterceptor(context: Context) : Interceptor {
+class AuthInterceptor(private val context: Context) : Interceptor {
     private val tokenRepository = EntryPointAccessors
         .fromApplication<AuthInterceptorEntryPoint>(context)
         .getTokenRepository()
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val token = tokenRepository.getToken()
-        val newRequest = chain.request().newBuilder()
-            .addHeader(ACCESS_TOKEN_HEADER, ACCESS_TOKEN_FORMAT.format(token?.accessToken))
-            .build()
-        return chain.proceed(newRequest)
+        val tokenAddedRequest = chain.request().putAccessToken(token?.accessToken)
+
+        val response = chain.proceed(tokenAddedRequest)
+        if (response.isAccessTokenInvalid()) {
+            navigateToLogin()
+        }
+        return response
+    }
+
+    private fun Response.isAccessTokenInvalid(): Boolean = (code == 401)
+
+    private fun Request.putAccessToken(token: String?): Request =
+        putHeader(ACCESS_TOKEN_HEADER, ACCESS_TOKEN_FORMAT.format(token))
+
+    private fun Request.putHeader(
+        key: String,
+        value: String,
+    ): Request = newBuilder().addHeader(key, value).build()
+
+    private fun navigateToLogin() {
+        val loginStartIntent = Intent(context, LoginActivity::class.java)
+            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(loginStartIntent)
     }
 
     @EntryPoint
