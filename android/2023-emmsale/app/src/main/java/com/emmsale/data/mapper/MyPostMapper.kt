556f67fd885af881d0eea78df3a06172f86e5da5
ki960213 <ki960213@gmail.com>
2023-11-30 13:46:54 +0900

    refactor(MyPost): MyPost 데이터 모델 및 그와 관련된 Repository 제거

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/mapper/MyPostMapper.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/mapper/MyPostMapper.kt
@@ -3,7 +3,6 @@ package com.emmsale.data.mapper
 import com.emmsale.data.apiModel.response.MyPostResponse
 import com.emmsale.data.model.Event
 import com.emmsale.data.model.Member
-import com.emmsale.data.model.MyPost
 import com.emmsale.data.model.Recruitment
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -16,17 +15,6 @@ fun MyPostResponse.toData() = Recruitment(
     content = content,
     updatedDate = updatedAt.toLocalDate(),
 )
-
-fun List<MyPostResponse>.toMyPosts(): List<MyPost> = map { it.toMyPost() }
-fun MyPostResponse.toMyPost(): MyPost {
-    return MyPost(
-        postId = postId,
-        eventId = eventId,
-        eventName = eventName,
-        content = content,
-        updatedAt = updatedAt.toLocalDate(),
-    )
-}
 
 private fun String.toLocalDate(): LocalDate {
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
