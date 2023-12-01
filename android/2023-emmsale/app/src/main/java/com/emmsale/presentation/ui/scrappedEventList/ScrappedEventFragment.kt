d20c0513b3a4f355e6c30bad0962233763081048
chws0508 <chws0508@naver.com>
2023-12-02 00:31:48 +0900

    refactor : 스크랩 화면 NetworkViewModel 적용

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/scrappedEventList/ScrappedEventFragment.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/scrappedEventList/ScrappedEventFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.viewModels
 import com.emmsale.R
 import com.emmsale.databinding.FragmentScrappedEventBinding
 import com.emmsale.presentation.base.BaseFragment
+import com.emmsale.presentation.common.CommonUiEvent
 import com.emmsale.presentation.common.ScrollTopListener
+import com.emmsale.presentation.common.extension.showToast
 import com.emmsale.presentation.ui.eventDetail.EventDetailActivity
 import com.emmsale.presentation.ui.scrappedEventList.recyclerView.ScrappedEventAdapter
 import com.emmsale.presentation.ui.scrappedEventList.uiState.ScrappedEventUiState
@@ -23,16 +25,22 @@ class ScrappedEventFragment : BaseFragment<FragmentScrappedEventBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initBinding()
-        setUpScrappedEvents()
+        setupBinding()
+        observeScrappedEvents()
+        observeCommonUiEvent()
     }
 
     override fun onResume() {
         super.onResume()
-        viewModel.refresh()
+        if (viewModel.isFirstFetch) {
+            viewModel.fetchScrappedEvents()
+            viewModel.isFirstFetch = false
+        } else {
+            viewModel.refresh()
+        }
     }
 
-    private fun initBinding() {
+    private fun setupBinding() {
         binding.vm = viewModel
         binding.rvScrappedEvents.adapter = scrappedEventsAdapter
         binding.rvScrappedEvents.addOnScrollListener(
@@ -40,9 +48,18 @@ class ScrappedEventFragment : BaseFragment<FragmentScrappedEventBinding>() {
         )
     }
 
-    private fun setUpScrappedEvents() {
+    private fun observeScrappedEvents() {
         viewModel.scrappedEvents.observe(viewLifecycleOwner) { scrappedEvents ->
             scrappedEventsAdapter.submitList(scrappedEvents.list)
+        }
+    }
+
+    private fun observeCommonUiEvent() {
+        viewModel.commonUiEvent.observe(viewLifecycleOwner) { commonUiEvent ->
+            when (commonUiEvent) {
+                CommonUiEvent.RequestFailByNetworkError -> showToast(getString(R.string.all_network_error_message))
+                is CommonUiEvent.Unexpected -> showToast(getString(R.string.all_network_error_message))
+            }
         }
     }
 
