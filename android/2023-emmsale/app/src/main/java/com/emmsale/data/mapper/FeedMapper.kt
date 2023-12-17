5d0879e13b778d8b2ea20c826caaead1b265e870
chws0508 <chws0508@naver.com>
2023-12-17 18:23:45 +0900

    refactor : Feed가 Event를 가지도록수정

--- android/2023-emmsale/app/src/main/java/com/emmsale/data/mapper/FeedMapper.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/data/mapper/FeedMapper.kt
@@ -2,6 +2,7 @@ package com.emmsale.data.mapper
 
 import com.emmsale.BuildConfig
 import com.emmsale.data.apiModel.response.FeedResponse
+import com.emmsale.data.model.Event
 import com.emmsale.data.model.Feed
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -10,7 +11,10 @@ fun List<FeedResponse>.toData(): List<Feed> = map { it.toData() }
 
 fun FeedResponse.toData(): Feed = Feed(
     id = id,
-    eventId = eventId,
+    event = Event(
+        id = eventId,
+        name = eventName,
+    ),
     title = title,
     content = content,
     writer = writer.toData(),
