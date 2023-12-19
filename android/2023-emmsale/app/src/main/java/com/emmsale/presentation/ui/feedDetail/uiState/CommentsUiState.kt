37b0cade0a7f229c2c1e3159327dbb04d2f3a5cd
buna <tmdgh1592@naver.com>
2023-12-19 18:14:14 +0900

    refactor: 라인 포맷팅

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/feedDetail/uiState/CommentsUiState.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/feedDetail/uiState/CommentsUiState.kt
@@ -6,21 +6,20 @@ data class CommentsUiState(val commentUiStates: List<CommentUiState> = emptyList
 
     constructor(uid: Long, comments: List<Comment>) : this(
         comments.flatMap { comment ->
-            listOf(CommentUiState.create(uid, comment)) +
-                    comment.childComments.map { childComment ->
-                        CommentUiState.create(uid, childComment)
-                    }
+            listOf(
+                CommentUiState.create(uid, comment),
+            ) + comment.childComments.map { childComment ->
+                CommentUiState.create(uid, childComment)
+            }
         },
     )
 
     constructor(uid: Long, comment: Comment) : this(
-        listOf(CommentUiState.create(uid, comment)) +
-                comment.childComments.map { childComment ->
-                    CommentUiState.create(
-                        uid,
-                        childComment,
-                    )
-                },
+        listOf(
+            CommentUiState.create(uid, comment),
+        ) + comment.childComments.map { childComment ->
+            CommentUiState.create(uid, childComment)
+        },
     )
 
     val size: Int = commentUiStates.size
