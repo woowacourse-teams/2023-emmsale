1f98e56683b5f3f83dc58b7f38be3267e9fb5331
토마스 <123928686+ki960213@users.noreply.github.com>
2023-12-29 21:41:33 +0900

    merge: 지도 앱에 장소 검색하는 기능 추가 (#878)
    Related to: #878

--- android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/eventDetailInfo/EventInfoFragment.kt
+++ android/2023-emmsale/app/src/main/java/com/emmsale/presentation/ui/eventDetailInfo/EventInfoFragment.kt
@@ -1,6 +1,9 @@
 package com.emmsale.presentation.ui.eventDetailInfo
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
@@ -9,6 +12,7 @@ import com.emmsale.databinding.FragmentEventInformationBinding
 import com.emmsale.presentation.base.BaseFragment
 import com.emmsale.presentation.common.firebase.analytics.FirebaseAnalyticsDelegate
 import com.emmsale.presentation.common.firebase.analytics.FirebaseAnalyticsDelegateImpl
+import com.emmsale.presentation.common.views.InfoDialog
 import com.emmsale.presentation.ui.eventDetail.EventDetailViewModel
 import com.emmsale.presentation.ui.eventDetailInfo.recyclerView.EventInfoImageAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,8 +31,33 @@ class EventInfoFragment :
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.vm = viewModel
+
+        setupDataBinding()
         setUpInformationUrls()
+    }
+
+    private fun setupDataBinding() {
+        binding.vm = viewModel
+        binding.onMapSearchButtonClick = ::navigateToMapApp
+    }
+
+    private fun navigateToMapApp(location: String) {
+        val uri = Uri.parse("geo:0,0?q=$location")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+
+        runCatching { startActivity(intent) }
+            .onFailure {
+                if (it is ActivityNotFoundException) showNotFoundMapAppDialog()
+            }
+    }
+
+    private fun showNotFoundMapAppDialog() {
+        InfoDialog(
+            context = requireContext(),
+            title = getString(R.string.eventInfo_not_found_map_app_dialog_title),
+            message = getString(R.string.eventInfo_not_found_map_app_dialog_message),
+            buttonLabel = getString(R.string.all_okay),
+        ).show()
     }
 
     private fun setUpInformationUrls() {
